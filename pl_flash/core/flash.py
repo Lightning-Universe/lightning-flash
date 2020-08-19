4236813ec87592e1d64ace5158287fa814bf6da4
Teddy Koker <teddy.koker@gmail.com>
2020-08-19 11:34:40 -0400

    remove batch_size arg from flash

--- pl_flash/core/flash.py
+++ pl_flash/core/flash.py
@@ -34,7 +34,6 @@ class Flash(LightningModule):
         model: torch.nn.Module,
         loss: Union[Callable, torch.nn.Module, Mapping, Sequence],
         metrics: Union[Callable, torch.nn.Module, Mapping, Sequence, None] = None,
-        batch_size: int = 8,
         learning_rate: float = 1e-3,
         optimizer: Union[Type[Optimizer], str] = "Adam",
     ):
@@ -69,9 +68,13 @@ class Flash(LightningModule):
             if isinstance(functions, Sequence):
                 functions = {get_name(func): resolve_str(func) for func in functions}
 
-            if isinstance(functions, Mapping) and not isinstance(functions, torch.nn.ModuleDict):
+            if isinstance(functions, Mapping) and not isinstance(
+                functions, torch.nn.ModuleDict
+            ):
                 functions = {k: resolve_str(v) for k, v in functions.items()}
-                if any([isinstance(func, torch.nn.Module) for func in functions.values()]):
+                if any(
+                    [isinstance(func, torch.nn.Module) for func in functions.values()]
+                ):
                     functions = torch.nn.ModuleDict(functions)
             elif isinstance(functions, torch.nn.Module):
                 functions = torch.nn.ModuleDict({get_name(functions): functions})
@@ -90,7 +93,6 @@ class Flash(LightningModule):
         super().__init__()
         self.save_hyperparameters()
         self.model = model
-        self.batch_size = batch_size
 
         if isinstance(optimizer, str):
             optimizer = getattr(torch.optim, optimizer)
@@ -107,15 +109,23 @@ class Flash(LightningModule):
         return self.optimizer_cls(self.parameters(), lr=self.learning_rate)
 
     def compute_loss(
-        self, y_hat: torch.Tensor, y: torch.tensor, prefix: str = "train", sep: str = "/",
+        self,
+        y_hat: torch.Tensor,
+        y: torch.tensor,
+        prefix: str = "train",
+        sep: str = "/",
     ):
         loss_dict = self.compute_dict(self.losses, y_hat, y, prefix, sep)
         return sum(loss_dict.values()), loss_dict
 
-    def compute_metrics(self, y_hat: torch.Tensor, y: torch.tensor, prefix: str = "val", sep: str = "/"):
+    def compute_metrics(
+        self, y_hat: torch.Tensor, y: torch.tensor, prefix: str = "val", sep: str = "/"
+    ):
         return self.compute_dict(self.metrics, y_hat, y, prefix, sep)
 
-    def _step(self, batch: Union[Sequence, Mapping], batch_idx: int, prefix: str) -> Union[EvalResult, TrainResult]:
+    def _step(
+        self, batch: Union[Sequence, Mapping], batch_idx: int, prefix: str
+    ) -> Union[EvalResult, TrainResult]:
 
         x, y = self.unpack_batch(batch)
         y_hat = self(x)
@@ -129,7 +139,11 @@ class Flash(LightningModule):
         elif prefix == "test":
             result = EvalResult()
         else:
-            raise ValueError("Expected prefix to be one of {{train, val, test}} but got {}".format(prefix))
+            raise ValueError(
+                "Expected prefix to be one of {{train, val, test}} but got {}".format(
+                    prefix
+                )
+            )
 
         for k, v in loss_dict.items():
             result.log(k, v, on_epoch=True, on_step=True)
@@ -159,7 +173,11 @@ class Flash(LightningModule):
             x, y = self.unpack_batch_mapping(batch)
 
         else:
-            raise TypeError("Expected Type of sequence or mapping for batch, got {}".format(type(batch).__name__))
+            raise TypeError(
+                "Expected Type of sequence or mapping for batch, got {}".format(
+                    type(batch).__name__
+                )
+            )
 
         return x, y
 
@@ -172,5 +190,9 @@ class Flash(LightningModule):
         return (batch["x"], batch["y"])
 
     @staticmethod
-    def compute_dict(functions, y_hat: torch.Tensor, y: torch.Tensor, prefix: str = "val", sep="/") -> dict:
-        return {f"{prefix}{sep}{name}": func(y_hat, y) for name, func in functions.items()}
+    def compute_dict(
+        functions, y_hat: torch.Tensor, y: torch.Tensor, prefix: str = "val", sep="/"
+    ) -> dict:
+        return {
+            f"{prefix}{sep}{name}": func(y_hat, y) for name, func in functions.items()
+        }
