336772b36f512143c9d01797a80da75913f46e83
Teddy Koker <teddy.koker@gmail.com>
2020-08-20 18:08:09 -0400

    ImageClassificationData.from_folders, docs, tests (#18)
    * imageclassificationdata.from_folders, docs, tests

--- pl_flash/vision/classification_datamodule.py
+++ pl_flash/vision/classification_datamodule.py
@@ -1,6 +1,7 @@
 from torch.utils.data import DataLoader, Dataset
 from pytorch_lightning import LightningDataModule
 
+from torchvision.datasets import ImageFolder
 from PIL import Image
 
 
@@ -28,6 +29,8 @@ class _FilepathDataset(Dataset):
 
 
 class ImageClassificationData(LightningDataModule):
+    """Data module for image classification tasks."""
+
     def __init__(
         self,
         train_ds: Dataset,
@@ -36,42 +39,53 @@ class ImageClassificationData(LightningDataModule):
         batch_size=64,
         num_workers=4,
     ):
-        super().__init__()
-        self.train_ds = train_ds
-        self.valid_ds = valid_ds
-        self.test_ds = test_ds
+        """
+        Initialize ImageClassificationData
 
-        if self.valid_ds is not None:
+        Args:
+            train_ds: Dataset for training.
+            valid_ds (optional): Dataset for validation.
+            test_ds (optional): Dataset for testing.
+            batch_size (optional): Batch size for data loading.
+            num_workers (optional): Number of workers for dataloaders, see
+                https://pytorch.org/docs/stable/data.html#multi-process-data-loading
+        """
+        super().__init__()
+        self._train_ds = train_ds
+        self._valid_ds = valid_ds
+        self._test_ds = test_ds
+
+        if self._valid_ds is not None:
             self.val_dataloader = self._val_dataloader
 
-        if self.test_ds is not None:
+        if self._test_ds is not None:
             self.test_dataloader = self._test_dataloader
 
-        self.batch_size = batch_size
-        self.num_workers = num_workers
+        self._batch_size = batch_size
+        self._num_workers = num_workers
 
     def train_dataloader(self):
         return DataLoader(
-            self.train_ds,
-            batch_size=self.batch_size,
+            self._train_ds,
+            batch_size=self._batch_size,
             shuffle=True,
-            num_workers=self.num_workers,
+            num_workers=self._num_workers,
             pin_memory=True,
         )
 
     def _val_dataloader(self):
         return DataLoader(
-            self.valid_ds,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
+            self._valid_ds,
+            batch_size=self._batch_size,
+            num_workers=self._num_workers,
             pin_memory=True,
         )
 
     def _test_dataloader(self):
         return DataLoader(
-            self.test_ds,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
+            self._test_ds,
+            batch_size=self._batch_size,
+            num_workers=self._num_workers,
             pin_memory=True,
         )
 
@@ -89,7 +103,26 @@ class ImageClassificationData(LightningDataModule):
         loader=pil_loader,
         batch_size=64,
     ):
-        # TODO: DOCSTRING
+        """
+        Creates a ImageClassificationData object from lists of image filepaths and labels
+
+        Args:
+            train_filepaths (list): list of image filepaths for training set.
+            train_labels (list): List of labels for training set.
+            train_transform (callable, optional): Image transform to use for training set.
+            valid_filepaths (list, optional): List of image filepaths for validation set.
+            valid_labels (list, optional): List of labels for validation set.
+            valid_transform (callable, optional): Image transform to use for validation and test set.
+            test_filepaths (list, optional): List of image filepaths for test set.
+            test_labels (list, optional): List of labels for test set.
+            loader (callbable, optional): A function to load an image given its path.
+            batch_size (optional): Batch size for data loading.
+
+        Examples::
+            >>> img_data = ImageClassificationData.from_filepaths(["a.png", "b.png"], [0, 1])
+
+        """
+
         train_ds = _FilepathDataset(
             train_filepaths, train_labels, train_transform, loader
         )
@@ -102,6 +135,58 @@ class ImageClassificationData(LightningDataModule):
         test_ds = (
             _FilepathDataset(test_filepaths, test_labels, valid_transform, loader)
             if test_filepaths is not None
+            else None
+        )
+
+        return cls(train_ds, valid_ds, test_ds, batch_size)
+
+    @classmethod
+    def from_folders(
+        cls,
+        train_folder,
+        train_transform=None,
+        valid_folder=None,
+        valid_transform=None,
+        test_folder=None,
+        loader=pil_loader,
+        batch_size=64,
+    ):
+        """
+        Creates a ImageClassificationData object from folders of images arranged in this way:
+
+        ::
+        train/dog/xxx.png
+        train/dog/xxy.png
+        train/dog/xxz.png
+
+        train/cat/123.png
+        train/cat/nsdf3.png
+        train/cat/asd932{_}.png
+
+        Args:
+            train_folder (path): Path to training folder.
+            train_transform (callable, optional): Image transform to use for training set.
+            valid_folder (path, optional): Path to validation folder.
+            valid_transform (callable, optional): Image transform to use for validation and test set.
+            test_folder (path, optional): Path to test folder.
+            loader (callbable, optional): A function to load an image given its path.
+            batch_size (optional): Batch size for data loading.
+
+        Examples::
+            >>> img_data = ImageClassificationData.from_folders("train/") # doctest: +SKIP
+
+        """
+
+        train_ds = ImageFolder(train_folder, train_transform, loader=loader)
+        valid_ds = (
+            ImageFolder(valid_folder, valid_transform, loader=loader)
+            if valid_folder is not None
+            else None
+        )
+
+        test_ds = (
+            ImageFolder(test_folder, valid_transform, loader=loader)
+            if test_folder is not None
             else None
         )
 
--- tests/vision/test_classification_datamodule.py
+++ tests/vision/test_classification_datamodule.py
@@ -1,4 +1,6 @@
 from pl_flash.vision import ImageClassificationData
+from pathlib import Path
+
 import torch
 
 
@@ -28,6 +30,45 @@ def test_from_filepaths(tmpdir):
         valid_labels=[0, 1],
         test_filepaths=["e", "f"],
         test_labels=[0, 1],
+        loader=_dummy_image_loader,
+        batch_size=1,
+    )
+
+    imgs, labels = next(iter(img_data.val_dataloader()))
+    assert imgs.shape == (1, 3, 64, 64)
+    assert labels.shape == (1,)
+
+    imgs, labels = next(iter(img_data.test_dataloader()))
+    assert imgs.shape == (1, 3, 64, 64)
+    assert labels.shape == (1,)
+
+
+def test_from_folders(tmpdir):
+    train_dir = Path(tmpdir / "train")
+    train_dir.mkdir()
+
+    (train_dir / "a").mkdir()
+    Path(train_dir / "a" / "1.png").touch()
+    Path(train_dir / "a" / "2.png").touch()
+
+    (train_dir / "b").mkdir()
+    Path(train_dir / "b" / "1.png").touch()
+    Path(train_dir / "b" / "2.png").touch()
+
+    img_data = ImageClassificationData.from_folders(
+        train_dir, loader=_dummy_image_loader, batch_size=1
+    )
+    imgs, labels = next(iter(img_data.train_dataloader()))
+    assert imgs.shape == (1, 3, 64, 64)
+    assert labels.shape == (1,)
+
+    assert img_data.val_dataloader() is None
+    assert img_data.test_dataloader() is None
+
+    img_data = ImageClassificationData.from_folders(
+        train_dir,
+        valid_folder=train_dir,
+        test_folder=train_dir,
         loader=_dummy_image_loader,
         batch_size=1,
     )
