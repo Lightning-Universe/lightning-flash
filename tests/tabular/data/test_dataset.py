f7fd47bc22d86862ede433c0ba6ec48a14abff44
Teddy Koker <teddy.koker@gmail.com>
2021-01-19 15:48:04 -0500

    Allow empty numerical or categorical columns for tabular data (#61)
    * update readme
    
    * allow empty numerical or categorical columns
    
    * fix readme

--- tests/tabular/data/test_dataset.py
+++ tests/tabular/data/test_dataset.py
@@ -13,6 +13,20 @@ TEST_DF = pd.DataFrame(
     }
 )
 
+TEST_DF_NO_NUM = pd.DataFrame(
+    data={
+        "category": [0, 1, 2, 1, 0, 2],
+        "label": [0, 1, 0, 1, 0, 1],
+    }
+)
+
+TEST_DF_NO_CAT = pd.DataFrame(
+    data={
+        "category": [0, 1, 2, 1, 0, 2],
+        "label": [0, 1, 0, 1, 0, 1],
+    }
+)
+
 
 def test_pandas():
     df = TEST_DF.copy()
@@ -27,4 +41,36 @@ def test_pandas():
     (cat, num), target = ds[0]
     assert cat == np.array([0])
     assert np.allclose(num, np.array([0.0, 5.0]))
+    assert target == 0
+
+
+def test_pandas_no_cat():
+    df = TEST_DF.copy()
+    ds = PandasDataset(
+        df,
+        cat_cols=[],
+        num_cols=["scalar_a", "scalar_b"],
+        target_col="label",
+        regression=False,
+    )
+    assert len(ds) == 6
+    (cat, num), target = ds[0]
+    assert cat.size == 0
+    assert np.allclose(num, np.array([0.0, 5.0]))
+    assert target == 0
+
+
+def test_pandas_no_num():
+    df = TEST_DF.copy()
+    ds = PandasDataset(
+        df,
+        cat_cols=["category"],
+        num_cols=[],
+        target_col="label",
+        regression=False,
+    )
+    assert len(ds) == 6
+    (cat, num), target = ds[0]
+    assert cat == np.array([0])
+    assert num.size == 0
     assert target == 0
