7d25285ccedcb1daf9ed98f779d1e676af099f22
chaton <thomas@grid.ai>
2021-01-28 16:41:28 +0000

    Add download progress bar
    * add progress_bar
    
    * add progress bar on data download
    
    * update
    
    * add test

--- flash/core/data/utils.py
+++ flash/core/data/utils.py
@@ -1,8 +1,53 @@
+import os.path
+import zipfile
 from io import BytesIO
 from urllib.request import urlopen, urlretrieve
 from zipfile import ZipFile
 
+import requests
 import torch
+from tqdm.auto import tqdm as tq
+
+
+# Code taken from: https://gist.github.com/ruxi/5d6803c116ec1130d484a4ab8c00c603
+# __author__  = "github.com/ruxi"
+# __license__ = "MIT"
+def download_file(url, path, verbose=False):
+    """
+    Download file with progressbar
+
+    Usage:
+        download_file('http://web4host.net/5MB.zip')
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+    local_filename = os.path.join(path, url.split('/')[-1])
+    r = requests.get(url, stream=True)
+    file_size = int(r.headers['Content-Length'])
+    chunk = 1
+    chunk_size = 1024
+    num_bars = int(file_size / chunk_size)
+    if verbose:
+        print(dict(file_size=file_size))
+        print(dict(num_bars=num_bars))
+
+    if not os.path.exists(local_filename):
+        with open(local_filename, 'wb') as fp:
+            for chunk in tq(
+                r.iter_content(chunk_size=chunk_size),
+                total=num_bars,
+                unit='KB',
+                desc=local_filename,
+                leave=True  # progressbar stays
+            ):
+                fp.write(chunk)
+
+    if '.zip' in local_filename:
+        if os.path.exists(local_filename):
+            with zipfile.ZipFile(local_filename, 'r') as zip_ref:
+                zip_ref.extractall(path)
+
+    return
 
 
 def download_data(url, path="data/"):
@@ -19,51 +64,7 @@ def download_data(url, path="data/"):
 
     """
 
-    if ".zip" in url:
-        download_zip_data(url, path)
-
-    else:
-        download_generic_data(url, path)
-
-
-def download_zip_data(url, path="data/"):
-    """
-    Example::
-
-        from flash.core.data import download_zip_data
-
-        # download titanic data
-        download_zip_data("https://pl-flash.core.data.s3.amazonaws.com/imdb.zip", "data/")
-
-    Args:
-        url: must end with .zip
-        path: path to download to
-
-    Returns:
-    """
-    with urlopen(url) as resp:
-        with ZipFile(BytesIO(resp.read())) as file:
-            file.extractall(path)
-
-
-def download_generic_data(url, path="data/"):
-    """
-    Downloads an arbitrary file.
-
-    Example::
-
-        from flash.core.data import download_csv_data
-
-        # download titanic data
-        download_csv_data("https://pl-flash.core.data.s3.amazonaws.com/titanic.csv", "titanic.csv")
-
-    Args:
-        url: must end with .csv
-        path: path to download to (include the file name)
-
-    Returns:
-    """
-    urlretrieve(url, path)
+    download_file(url, path)
 
 
 def _contains_any_tensor(value, dtype=torch.Tensor):
