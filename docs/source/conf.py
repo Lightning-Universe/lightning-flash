c93259eff425d140967ef1433f1137074ad3fb7d
Teddy Koker <teddy.koker@gmail.com>
2021-01-29 13:34:36 -0500

    fix logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ sys.path.insert(0, os.path.abspath(_PATH_ROOT))
 builtins.__LIGHTNING_FLASH_SETUP__ = True
 import flash  # noqa: E402
 
+html_favicon = '_static/images/icon.svg'
+
 # -- Project information -----------------------------------------------------
 
 project = "Flash"
@@ -98,12 +100,10 @@ html_theme_options = {
     "logo_only": False,
 }
 
-html_logo = "_images/lightning_logo.svg"
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_images"]
+html_static_path = ["_static"]
 
 html_css_files = []
 
