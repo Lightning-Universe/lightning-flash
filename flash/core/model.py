00f10df00ddb199cbaa9f9a693330bf46d130423
tchaton <thomas@grid.ai>
2021-02-01 18:06:32 +0000

    removing todo

--- flash/core/model.py
+++ flash/core/model.py
@@ -95,7 +95,6 @@ class Task(pl.LightningModule):
     @property
     @contextmanager
     def predict_context(self):
-        # todo: Move this within Lightning.
         try:
             self.eval()
             torch.set_grad_enabled(False)
