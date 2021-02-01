55142ca44f8c9777bead102adaea2a471b3e93dc
Kaushik Bokka <kaushikbokka@gmail.com>
2021-02-02 01:41:12 +0530

    fix imports

--- flash/vision/classification/components/backbones.py
+++ flash/vision/classification/components/backbones.py
@@ -2,16 +2,11 @@ from typing import Tuple
 
 import torch.nn as nn
 
-from flash.vision.classification.components.torchvision_model_zoo import TORCHVISION_MODEL_ZOO
+from flash.vision.classification.components import TORCHVISION_MODEL_ZOO
 
 
 def torchvision_backbone_and_num_features(model_name: str, pretrained: bool = True) -> Tuple[nn.Module, int]:
-    """
-    Returns CNN backbone & it's final num of features from Torchvision supported models.
-    Args:
-        model_name: Name of the model. E.g. resnet18
-        pretrained: Pretrained weights on the ImageNet dataset
-    """
+
     model = TORCHVISION_MODEL_ZOO[model_name]
     model = model(pretrained=pretrained)
     if model_name in ["mobilenet_v2", "vgg11", "vgg13", "vgg16", "vgg19"]:
