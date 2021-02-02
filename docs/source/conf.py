bcacebd57f15b064034dcc02741d0fe2487f3c2b
tchaton <thomas@grid.ai>
2021-02-02 16:19:04 +0000

    add filelock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -137,6 +137,7 @@ PACKAGE_MAPPING = {
     'PyYAML': 'yaml',
     'rouge-score': 'rouge_score',
     'pytorch-lightning-bolts': 'pl_bolts',
+    'filelock': 'filelock',
 }
 MOCK_PACKAGES = []
 if SPHINX_MOCK_REQUIREMENTS:
