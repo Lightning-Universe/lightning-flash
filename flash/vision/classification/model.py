faba990cf56b928eb2abc7306176c2cac1a08ee9
Teddy Koker <teddy.koker@gmail.com>
2021-02-03 19:05:19 -0500

    reorg

--- flash/vision/classification/model.py
+++ flash/vision/classification/model.py
@@ -19,7 +19,7 @@ from torch import nn
 from torch.nn import functional as F
 
 from flash.core.classification import ClassificationTask
-from flash.vision.backbones import torchvision_backbone_and_num_features
+from flash.vision.backbones import backbone_and_num_features
 from flash.vision.classification.data import ImageClassificationData, ImageClassificationDataPipeline
 
 
@@ -57,7 +57,7 @@ class ImageClassifier(ClassificationTask):
 
         self.save_hyperparameters()
 
-        self.backbone, num_features = torchvision_backbone_and_num_features(backbone, pretrained)
+        self.backbone, num_features = backbone_and_num_features(backbone, pretrained)
 
         self.head = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)),
