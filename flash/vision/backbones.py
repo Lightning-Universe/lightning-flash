d8e871b250def00132061e8a89f96bbd5424ccd9
Mabu Manaileng <ManailengMJ@gmail.com>
2021-02-07 14:52:07 +0200

    remove densenet161 duplicate in DENSENET_MODELS

--- flash/vision/backbones.py
+++ flash/vision/backbones.py
@@ -26,7 +26,7 @@ ROOT_S3_BUCKET = "https://pl-bolts-weights.s3.us-east-2.amazonaws.com"
 MOBILENET_MODELS = ["mobilenet_v2"]
 VGG_MODELS = ["vgg11", "vgg13", "vgg16", "vgg19"]
 RESNET_MODELS = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152", "resnext50_32x4d", "resnext101_32x8d"]
-DENSENET_MODELS = ["densenet121", "densenet169", "densenet161", "densenet161"]
+DENSENET_MODELS = ["densenet121", "densenet169", "densenet161"]
 TORCHVISION_MODELS = MOBILENET_MODELS + VGG_MODELS + RESNET_MODELS + DENSENET_MODELS
 
 BOLTS_MODELS = ["simclr-imagenet", "swav-imagenet"]
