84c5dec35cbe3da415306b22b625e5963aac822f
Kaushik Bokka <kaushikbokka@gmail.com>
2021-02-08 23:25:15 +0530

    use download_data with coco128 url

--- flash_examples/finetuning/image_detection.py
+++ flash_examples/finetuning/image_detection.py
@@ -1,10 +1,24 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import flash
+from flash.core.data import download_data
 from flash.vision.detection.data import ImageDetectionData
-from flash.vision.detection.dataset import coco128_data_download
 from flash.vision.detection.model import ImageDetector
 
 # 1. Download the data
-coco128_data_download("data/")
+# Dataset Credit: https://www.kaggle.com/ultralytics/coco128
+download_data("https://github.com/zhiqwang/yolov5-rt-stack/releases/download/v0.3.0/coco128.zip", "data/")
 
 # 2. Load the Data
 datamodule = ImageDetectionData.from_coco(
