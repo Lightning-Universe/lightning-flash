a6462abcc5450cb872c913072b3b56ba65b33f8a
Jirka Borovec <jirka.borovec@seznam.cz>
2021-02-08 13:49:16 +0100

    prepare v0.1.1

--- flash/core/model.py
+++ flash/core/model.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Callable, Dict, Mapping, Optional, Sequence, Type, Union
 import functools
+from typing import Any, Callable, Dict, Mapping, Optional, Sequence, Type, Union
 
 import pytorch_lightning as pl
 import torch
