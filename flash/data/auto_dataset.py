2e7bc4b0040e164cf5b1b0333563460756fc608a
thomas chaton <thomas@grid.ai>
2021-03-22 19:11:09 +0000

    Update flash/data/auto_dataset.py
    Co-authored-by: Kaushik B <45285388+kaushikb11@users.noreply.github.com>

--- flash/data/auto_dataset.py
+++ flash/data/auto_dataset.py
@@ -21,7 +21,7 @@ class AutoDataset(torch.utils.data.Dataset):
     STAGES = ("train", "test", "val", "predict")
     DATASET_KEY = "dataset"
     """
-        This class is used to encapsultate a Preprocess Object ``load_data`` and ``load_sample`` functions.
+        This class is used to encapsulate a Preprocess Object ``load_data`` and ``load_sample`` functions.
         ``load_data`` will be called within the ``__init__`` function of the AutoDataset if ``running_stage``
         is provided and ``load_sample`` within ``__getitem__`` function.
     """
