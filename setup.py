5e94d75add8ad2377f822c12f4460e80e50a8790
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-25 14:07:18 +0100

    refactoring setup

--- setup.py
+++ setup.py
@@ -2,21 +2,24 @@
 import os
 
 # Always prefer setuptools over distutils
+import sys
+
 from setuptools import find_packages, setup
 
 try:
-    import builtins
+    from flash import info, setup_tools
 except ImportError:
-    import __builtin__ as builtins  # type: ignore
+    # alternative https://stackoverflow.com/a/67692/4521646
+    sys.path.append("flash")
+    import info
+    import setup_tools
 
 # https://packaging.python.org/guides/single-sourcing-package-version/
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
 
 _PATH_ROOT = os.path.dirname(__file__)
-builtins.__LIGHTNING_FLASH_SETUP__ = True
 
-import flash  # noqa: E402
-from flash.setup_tools import _load_readme_description, _load_requirements  # noqa: E402
+long_description = setup_tools._load_readme_description(_PATH_ROOT)
 
 # https://packaging.python.org/discussions/install-requires-vs-requirements /
 # keep the meta-data here for simplicity in reading this file... it's not obvious
@@ -25,22 +28,22 @@ from flash.setup_tools import _load_readme_description, _load_requirements  # no
 # engineer specific practices
 setup(
     name="lightning-flash",
-    version=flash.__version__,
-    description=flash.__docs__,
-    author=flash.__author__,
-    author_email=flash.__author_email__,
-    url=flash.__homepage__,
+    version=info.__version__,
+    description=info.__docs__,
+    author=info.__author__,
+    author_email=info.__author_email__,
+    url=info.__homepage__,
     download_url="https://github.com/PyTorchLightning/lightning-flash",
-    license=flash.__license__,
+    license=info.__license__,
     packages=find_packages(exclude=["tests", "docs"]),
-    long_description=_load_readme_description(_PATH_ROOT),
+    long_description=setup_tools._load_readme_description(_PATH_ROOT),
     long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
     keywords=["deep learning", "pytorch", "AI"],
     python_requires=">=3.6",
     setup_requires=[],
-    install_requires=_load_requirements(_PATH_ROOT, file_name='requirements.txt'),
+    install_requires=setup_tools._load_requirements(_PATH_ROOT, file_name='requirements.txt'),
     project_urls={
         "Bug Tracker": "https://github.com/PyTorchLightning/lightning-flash/issues",
         "Documentation": "https://lightning-flash.rtfd.io/en/latest/",
