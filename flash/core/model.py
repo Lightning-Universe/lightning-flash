96f1d20f97a9b21b9567e738364cf3d5e2241348
Aniket Maurya <theaniketmaurya@gmail.com>
2021-04-13 00:46:14 +0530

    Update flash/core/model.py
    Co-authored-by: thomas chaton <thomas@grid.ai>

--- flash/core/model.py
+++ flash/core/model.py
@@ -165,7 +165,7 @@ class Task(pl.LightningModule):
     ) -> Union[Tuple[Tuple[torch.optim.Optimizer], Any], Tuple[torch.optim.Optimizer]]:
         optimizers = self.optimizer_cls(filter(lambda p: p.requires_grad, self.parameters()), lr=self.learning_rate),
         if self.scheduler:
-            return optimizers, self.schedulers
+            return optimizers, self.scheduler(optimizer=optimizer)
         return optimizers
 
     @property
