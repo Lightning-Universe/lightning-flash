17bfe73e3690c58ee8e14fe0ea7c0bbe3a028d94
tchaton <thomas@grid.ai>
2021-04-16 18:26:12 +0100

    update

--- tests/examples/test_scripts.py
+++ tests/examples/test_scripts.py
@@ -73,7 +73,6 @@ def test_example(tmpdir, folder, file):
     run_test(str(root / "flash_examples" / folder / file))
 
 
-@pytest.mark.skipif(reason="CI bug")
 def test_generic_example(tmpdir):
     run_test(str(root / "flash_examples" / "generic_task.py"))
 
