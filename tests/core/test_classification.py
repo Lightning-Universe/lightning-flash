a408f60647cfbcf1cf52cd1aff97cd7bfe8c82cd
Ethan Harris <ethanwharris@gmail.com>
2021-04-21 08:40:17 +0100

    Update test_classification

--- tests/core/test_classification.py
+++ tests/core/test_classification.py
@@ -18,8 +18,22 @@ from flash.core.classification import Classes, Labels, Logits, Probabilities
 
 def test_classification_serializers():
     example_output = torch.tensor([-0.1, 0.2, 0.3])  # 3 classes
+    labels = ['class_1', 'class_2', 'class_3']
 
-    assert Logits().serialize(example_output) == example_output.tolist()
+    assert torch.allclose(torch.tensor(Logits().serialize(example_output)), example_output)
     assert torch.allclose(torch.tensor(Probabilities().serialize(example_output)), torch.softmax(example_output, -1))
     assert Classes().serialize(example_output) == 2
-    assert Labels(['class_1', 'class_2', 'class_3']).serialize(example_output) == 'class_3'
+    assert Labels(labels).serialize(example_output) == 'class_3'
+
+
+def test_classification_serializers_multi_label():
+    example_output = torch.tensor([-0.1, 0.2, 0.3])  # 3 classes
+    labels = ['class_1', 'class_2', 'class_3']
+
+    assert torch.allclose(torch.tensor(Logits(multi_label=True).serialize(example_output)), example_output)
+    assert torch.allclose(
+        torch.tensor(Probabilities(multi_label=True).serialize(example_output)),
+        torch.sigmoid(example_output),
+    )
+    assert Classes(multi_label=True).serialize(example_output) == [1, 2]
+    assert Labels(labels, multi_label=True).serialize(example_output) == ['class_2', 'class_3']
