eab18c4d70f10d350e4085669c60e3c8f318d1d0
tchaton <thomas@grid.ai>
2021-05-02 17:15:00 +0100

    update

--- flash/vision/classification/model.py
+++ flash/vision/classification/model.py
@@ -105,7 +105,6 @@ class ImageClassifier(ClassificationTask):
 
         head = head(num_features, num_classes) if isinstance(head, FunctionType) else head
         self.head = head or nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten(),
             nn.Linear(num_features, num_classes),
         )
