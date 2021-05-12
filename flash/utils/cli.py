216397ef964f166077f7aad0aa5ce4f81bac5bf5
Carlos Mocholi <carlossmocholi@gmail.com>
2021-05-12 15:44:42 +0200

    FlashCLI

---
+++ flash/utils/cli.py
@@ -0,0 +1,87 @@
+from typing import Any, Callable, Optional, Type, Union
+
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.cli import LightningArgumentParser, LightningCLI
+
+import flash
+from flash.data.data_source import DefaultDataSources
+
+
+class FlashCLI(LightningCLI):
+
+    def __init__(
+        self,
+        model_class: Type[pl.LightningModule],
+        datamodule_class: Type[flash.DataModule],
+        trainer_class: Type[pl.Trainer] = flash.Trainer,
+        datasource: Optional[Union[str, Callable]] = None,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Flash's extension of the :class:`pytorch_lightning.utilities.cli.LightningCLI`
+
+        Args:
+            model_class: The :class:`pytorch_lightning.LightningModule` class to train on.
+            datamodule_class: The :class:`~flash.data.data_module.DataModule` class.
+            trainer_class: An optional extension of the :class:`pytorch_lightning.Trainer` class.
+            datasource: Use this if your ``DataModule`` is created using a classmethod. Any of:
+                - ``None``. The ``datamodule_class.__init__`` signature will be used.
+                - ``str``. One of :class:`~flash.data.data_source.DefaultDataSources`. This will use the signature of
+                    the corresponding ``DataModule.from_*`` method.
+                - ``Callable``. A custom method.
+            kwargs: See the parent arguments
+        """
+        super().__init__(model_class, datamodule_class=datamodule_class, trainer_class=trainer_class, **kwargs)
+        if isinstance(datasource, str):
+            self.datasource = DefaultDataSources(datasource)
+        self.datasource_fn: Optional[Callable] = None
+
+    def add_core_arguments_to_parser(self):
+        """Adds arguments from the core classes to the parser"""
+        self.parser.add_argument(
+            '--seed_everything',
+            type=Optional[int],
+            default=self.seed_everything_default,
+            help='Set to an int to run seed_everything with this value before classes instantiation',
+        )
+        self.parser.add_lightning_class_args(self.trainer_class, 'trainer')
+        trainer_defaults = {'trainer.' + k: v for k, v in self.trainer_defaults.items() if k != 'callbacks'}
+        self.parser.set_defaults(trainer_defaults)
+        self.parser.add_lightning_class_args(self.model_class, 'model', subclass_mode=self.subclass_mode_model)
+
+        # Modification to work with `DataSource`s
+        if self.datasource is None:
+            # Same as parent, use the `__init__`
+            self.parser.add_lightning_class_args(self.datamodule_class, 'data', subclass_mode=self.subclass_mode_data)
+            return
+
+        if callable(self.datasource):
+            # The user passed their own function, use it
+            self.datasource_fn = self.datasource
+
+        # Get the DataModule creation function with the data source provided
+        elif self.datasource == DefaultDataSources.FOLDERS:
+            self.datasource_fn = self.datamodule_class.from_folders
+        elif self.datasource == DefaultDataSources.FILES:
+            self.datasource_fn = self.datamodule_class.from_files
+        elif self.datasource == DefaultDataSources.NUMPY:
+            self.datasource_fn = self.datamodule_class.from_numpy
+        elif self.datasource == DefaultDataSources.TENSORS:
+            self.datasource_fn = self.datamodule_class.from_tensor
+        elif self.datasource == DefaultDataSources.CSV:
+            self.datasource_fn = self.datamodule_class.from_csv
+        elif self.datasource == DefaultDataSources.JSON:
+            self.datasource_fn = self.datamodule_class.from_json
+        else:
+            raise ValueError
+
+        self.parser.add_function_arguments(self.datasource_fn, nested_key='data')
+
+    def instantiate_datamodule(self) -> None:
+        """Instantiates the datamodule using self.config_init['data'] if given"""
+        if self.datasource_fn is not None:
+            self.datamodule = self.datasource_fn(**self.config_init['data'])
+        elif self.subclass_mode_data:
+            self.datamodule = self.config_init['data']
+        else:
+            self.datamodule = self.datamodule_class(**self.config_init.get('data', {}))
--- flash_examples/finetuning/image_classification_cli.py
+++ flash_examples/finetuning/image_classification_cli.py
@@ -14,25 +14,17 @@
 import torchvision
 from torch import nn
 
-import flash
-from flash import Trainer
 from flash.core.classification import Labels
 from flash.core.finetuning import FreezeUnfreeze
 from flash.data.utils import download_data
+from flash.utils.cli import FlashCLI
 from flash.vision import ImageClassificationData, ImageClassifier
 
 # 1. Download the data
 download_data("https://pl-flash-data.s3.amazonaws.com/hymenoptera_data.zip", "data/")
 
-# 2. Load the data
-datamodule = ImageClassificationData.from_folders(
-    train_folder="data/hymenoptera_data/train/",
-    val_folder="data/hymenoptera_data/val/",
-    test_folder="data/hymenoptera_data/test/",
-)
 
-
-# 3.a Optional: Register a custom backbone
+# 2.a Optional: Register a custom backbone
 # This is useful to create new backbone and make them accessible from `ImageClassifier`
 @ImageClassifier.backbones(name="resnet18")
 def fn_resnet(pretrained: bool = True):
@@ -44,35 +36,53 @@ def fn_resnet(pretrained: bool = True):
     return backbone, num_features
 
 
-# 3.b Optional: List available backbones
+# 2.b Optional: List available backbones
 print(ImageClassifier.available_backbones())
 
-# 4. Build the model
-model = ImageClassifier(backbone="resnet18", num_classes=datamodule.num_classes)
 
-# 5. Create the trainer.
-trainer = flash.Trainer(max_epochs=1, limit_train_batches=1, limit_val_batches=1)
+class ImageClassificationCLI(FlashCLI):
 
-# 6. Train the model
-trainer.finetune(model, datamodule=datamodule, strategy=FreezeUnfreeze(unfreeze_epoch=1))
+    def add_arguments_to_parser(self, parser):
+        parser.set_defaults({
+            'data.train_folder': 'data/hymenoptera_data/train/',
+            'data.val_folder': 'data/hymenoptera_data/val/',
+            'data.test_folder': 'data/hymenoptera_data/test/',
+        })
 
-# 7a. Predict what's on a few images! ants or bees?
+    def parse_arguments(self):
+        # ignore the fact that this is needed - might be a bug
+        self.config = self.parser.parse_args(_skip_check=True)
 
-# Serialize predictions as lables, automatically inferred from the training data in part 2.
-model.serializer = Labels()
+    def instantiate_datamodule(self):
+        # Link the num_classes for the model
+        self.config_init["model"]["num_classes"] = self.datamodule.num_classes
 
-predictions = model.predict([
+    def prepare_fit_kwargs(self):
+        super().prepare_fit_kwargs()
+        # TODO: expose the strategy arguments?
+        self.fit_kwargs["strategy"] = FreezeUnfreeze(unfreeze_epoch=1)
+
+    def fit(self):
+        self.trainer.finetune(**self.fit_kwargs)
+
+
+# 3. Build the model, datamodule, and trainer. Expose them through CLI. Fine-tune
+cli = ImageClassificationCLI(ImageClassifier, ImageClassificationData, datasource="paths")
+
+# 4. Save a checkpoint!
+cli.trainer.save_checkpoint("image_classification_model.pt")
+
+# 5a. Predict what's on a few images! ants or bees?
+# Serialize predictions as labels, automatically inferred from the training data in part 2.
+cli.model.serializer = Labels()
+predictions = cli.model.predict([
     "data/hymenoptera_data/val/bees/65038344_52a45d090d.jpg",
     "data/hymenoptera_data/val/bees/590318879_68cf112861.jpg",
     "data/hymenoptera_data/val/ants/540543309_ddbb193ee5.jpg",
 ])
 print(predictions)
 
+# 5b. Or generate predictions with a whole folder!
 datamodule = ImageClassificationData.from_folders(predict_folder="data/hymenoptera_data/predict/")
-
-# 7b. Or generate predictions with a whole folder!
-predictions = Trainer().predict(model, datamodule=datamodule)
+predictions = cli.trainer.predict(cli.model, datamodule=datamodule)
 print(predictions)
-
-# 8. Save it!
-trainer.save_checkpoint("image_classification_model.pt")
