2de1ddf1d787e4cb374777780435cbde6cb82596
tchaton <thomas@grid.ai>
2021-05-14 19:15:53 +0100

    update

--- tests/examples/test_scripts.py
+++ tests/examples/test_scripts.py
@@ -95,28 +95,39 @@ def run_test(filepath):
         pytest.param(
             "predict",
             "image_classification.py",
-            marks=pytest.mark.skipif(not _IMAGE_AVAILABLE, reason="image libraries aren't installed")
+            marks=pytest.mark.skipif(
+                not _IMAGE_AVAILABLE and not _TORCHVISION_GREATER_EQUAL_0_9, reason="image libraries aren't installed"
+            )
         ),
         pytest.param(
             "predict",
             "image_classification_multi_label.py",
-            marks=pytest.mark.skipif(not _IMAGE_AVAILABLE, reason="image libraries aren't installed")
+            marks=pytest.mark.skipif(
+                not _IMAGE_AVAILABLE and not _TORCHVISION_GREATER_EQUAL_0_9, reason="image libraries aren't installed"
+            )
         ),
         pytest.param(
             "predict",
             "semantic_segmentation.py",
-            marks=pytest.mark.skipif(not _IMAGE_AVAILABLE, reason="image libraries aren't installed")
+            marks=pytest.mark.skipif(
+                not _IMAGE_AVAILABLE and not _TORCHVISION_GREATER_EQUAL_0_9, reason="image libraries aren't installed"
+            )
         ),
         pytest.param(
             "predict",
             "tabular_classification.py",
-            marks=pytest.mark.skipif(not _TABULAR_AVAILABLE, reason="tabular libraries aren't installed")
+            marks=pytest.mark.skipif(
+                not _TABULAR_AVAILABLE and not _TORCHVISION_GREATER_EQUAL_0_9,
+                reason="tabular libraries aren't installed"
+            )
         ),
         # pytest.param("predict", "text_classification.py"),
         pytest.param(
             "predict",
             "image_embedder.py",
-            marks=pytest.mark.skipif(not _IMAGE_AVAILABLE, reason="image libraries aren't installed")
+            marks=pytest.mark.skipif(
+                not _IMAGE_AVAILABLE and not _TORCHVISION_GREATER_EQUAL_0_9, reason="image libraries aren't installed"
+            )
         ),
         pytest.param(
             "predict",
