f539a173d9c37eb7a32121cc7e5ff8e21bcc2c93
tchaton <thomas@grid.ai>
2021-05-14 17:27:40 +0100

    update

--- flash/core/utilities/imports.py
+++ flash/core/utilities/imports.py
@@ -1,4 +1,24 @@
-from pytorch_lightning.utilities.imports import _module_available
+from importlib.util import find_spec
+
+
+def _module_available(module_path: str) -> bool:
+    """
+    Check if a path is available in your environment
+
+    >>> _module_available('os')
+    True
+    >>> _module_available('bla.bla')
+    False
+    """
+    try:
+        return find_spec(module_path) is not None
+    except AttributeError:
+        # Python 3.6
+        return False
+    except ModuleNotFoundError:
+        # Python 3.7+
+        return False
+
 
 _TORCH_AVAILABLE = _module_available("torch")
 _PANDAS_AVAILABLE = _module_available("pandas")
