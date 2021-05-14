464a26c79d44a3da1b6aec1e1379671d212068cd
Philip Meier <github.pmeier@posteo.de>
2021-05-14 20:44:58 +0200

    add explanation to not supported phases

--- flash/vision/style_transfer/_utils.py
+++ flash/vision/style_transfer/_utils.py
@@ -4,4 +4,7 @@ __all__ = ["raise_not_supported"]
 
 
 def raise_not_supported(phase: str) -> NoReturn:
-    raise RuntimeError(f"Style transfer does not support a {phase} phase.")
+    raise RuntimeError(
+        f"Style transfer does not support a {phase} phase, "
+        f"since there is no metric to objectively determine the quality of a stylization."
+    )
