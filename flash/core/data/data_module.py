e1040b4110439bcb38c140ec5c6b4abc19021284
thomas chaton <thomas@grid.ai>
2021-05-17 14:08:35 +0100

    Update flash/core/data/data_module.py
    Co-authored-by: Ethan Harris <ewah1g13@soton.ac.uk>

--- flash/core/data/data_module.py
+++ flash/core/data/data_module.py
@@ -968,7 +968,7 @@ class DataModule(pl.LightningDataModule):
     ) -> 'DataModule':
         """Creates a :class:`~flash.core.data.data_module.DataModule` object from the given datasets using the
         :class:`~flash.core.data.data_source.DataSource`
-        of name :attr:`~flash.core.data.data_source.DefaultDataSources.CSV`
+        of name :attr:`~flash.core.data.data_source.DefaultDataSources.DATASET`
         from the passed or constructed :class:`~flash.core.data.process.Preprocess`.
 
         Args:
