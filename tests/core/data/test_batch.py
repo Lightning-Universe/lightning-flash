e50cd3a0556225b5d6ad4ab8088e443d55507d37
Ethan Harris <ethanwharris@gmail.com>
2021-05-21 10:57:02 +0100

    Updates str and tests

--- tests/core/data/test_batch.py
+++ tests/core/data/test_batch.py
@@ -11,11 +11,70 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import torch
-from torch.tensor import Tensor
-from torch.testing import assert_allclose
+from unittest.mock import Mock
 
-from flash.core.data.batch import default_uncollate
+import torch
+from pytorch_lightning.trainer.states import RunningStage
+from torch.testing import assert_allclose
+from torch.utils.data._utils.collate import default_collate
+
+from flash.core.data.batch import _PostProcessor, _PreProcessor, _Sequential, default_uncollate
+
+
+def test_sequential_str():
+    sequential = _Sequential(
+        Mock(name="preprocess"),
+        torch.softmax,
+        torch.as_tensor,
+        torch.relu,
+        RunningStage.TRAINING,
+        True,
+    )
+    assert str(sequential) == (
+        "_Sequential:\n"
+        "\t(pre_tensor_transform): FuncModule(softmax)\n"
+        "\t(to_tensor_transform): FuncModule(as_tensor)\n"
+        "\t(post_tensor_transform): FuncModule(relu)\n"
+        "\t(assert_contains_tensor): True\n"
+        "\t(stage): RunningStage.TRAINING"
+    )
+
+
+def test_preprocessor_str():
+    preprocessor = _PreProcessor(
+        Mock(name="preprocess"),
+        default_collate,
+        torch.relu,
+        torch.softmax,
+        RunningStage.TRAINING,
+        False,
+        True,
+    )
+    assert str(preprocessor) == (
+        "_PreProcessor:\n"
+        "\t(per_sample_transform): FuncModule(relu)\n"
+        "\t(collate_fn): FuncModule(default_collate)\n"
+        "\t(per_batch_transform): FuncModule(softmax)\n"
+        "\t(apply_per_sample_transform): False\n"
+        "\t(on_device): True\n"
+        "\t(stage): RunningStage.TRAINING"
+    )
+
+
+def test_postprocessor_str():
+    postprocessor = _PostProcessor(
+        default_uncollate,
+        torch.relu,
+        torch.softmax,
+        None,
+    )
+    assert str(postprocessor) == (
+        "_PostProcessor:\n"
+        "\t(per_batch_transform): FuncModule(relu)\n"
+        "\t(uncollate_fn): FuncModule(default_uncollate)\n"
+        "\t(per_sample_transform): FuncModule(softmax)\n"
+        "\t(serializer): None"
+    )
 
 
 class TestDefaultUncollate:
