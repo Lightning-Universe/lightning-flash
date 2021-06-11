e39293c3c8e71f4aa98666b1f4681149d935209d
tchaton <thomas@grid.ai>
2021-06-11 17:52:25 +0100

    remove todo

--- flash_examples/serve/segmentic_segmentation/inference_server.py
+++ flash_examples/serve/segmentic_segmentation/inference_server.py
@@ -11,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# todo: (tchaton) This script is currently not working.
-
 from flash.image import SemanticSegmentation
 
 model = SemanticSegmentation.load_from_checkpoint(
