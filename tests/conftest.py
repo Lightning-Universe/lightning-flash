cc09a8e43881f3a4690b5b6e2e3a9d96a3681f04
Ethan Harris <ethanwharris@gmail.com>
2021-06-18 08:38:04 +0100

    Fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,8 @@ import torch
 from pytest_mock import MockerFixture
 
 from flash.core.serve.decorators import uuid4  # noqa (used in mocker.patch)
-from flash.core.utilities.imports import _SERVE_TESTING, _TORCHVISION_AVAILABLE
+from flash.core.utilities.imports import _TORCHVISION_AVAILABLE
+from tests.helpers.utils import _SERVE_TESTING
 
 if _TORCHVISION_AVAILABLE:
     import torchvision
