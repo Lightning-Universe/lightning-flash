a53bbe42b5c68366b9949f1d53ae0ce4c3d1e74f
Akihiro Nitta <nitta@akihironitta.com>
2021-07-08 14:12:44 +0900

    Update docs

--- flash/core/data/process.py
+++ flash/core/data/process.py
@@ -462,6 +462,10 @@ class DefaultPreprocess(Preprocess):
 
 
 class Postprocess(Properties):
+    """
+    The :class:`~flash.core.data.process.Postprocess` encapsulates all the data processing logic that should run after
+    the model.
+    """
 
     def __init__(self, save_path: Optional[str] = None):
         super().__init__()
