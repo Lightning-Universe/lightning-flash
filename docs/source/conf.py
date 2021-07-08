ea2eb0dbaed2d87215551cde7a09935c8a95059f
Akihiro Nitta <nitta@akihironitta.com>
2021-07-08 15:17:58 +0900

    Add todo extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
-    # 'sphinx.ext.todo',
+    'sphinx.ext.todo',
     # 'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
