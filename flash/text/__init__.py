e74bd241c43029bf1185559da02760a4a7d5ee7b
Karthik Rangasai <karthikrangasai@gmail.com>
2021-07-12 15:49:24 +0530

    Added tests for the QuestionAnsweringData class

--- flash/text/__init__.py
+++ flash/text/__init__.py
@@ -1,5 +1,6 @@
 from flash.text.classification import TextClassificationData, TextClassifier  # noqa: F401
 from flash.text.seq2seq import (  # noqa: F401
+    QuestionAnsweringData,
     Seq2SeqData,
     Seq2SeqTask,
     SummarizationData,
