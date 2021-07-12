f7a86eae45786db8ef56b8b24f3e28e13d2be581
karthikrangasai <39360170+karthikrangasai@users.noreply.github.com>
2021-07-12 22:47:32 +0530

    Adding QuestionAnsweringTask class to the question answering task (#567)
    * Adding QuestionAnsweringTask class to the question answering task
    
    * Small changes based on pep8 guidelines
    
    Co-authored-by: Ethan Harris <ewah1g13@soton.ac.uk>

--- flash/text/__init__.py
+++ flash/text/__init__.py
@@ -1,6 +1,7 @@
 from flash.text.classification import TextClassificationData, TextClassifier  # noqa: F401
 from flash.text.seq2seq import (  # noqa: F401
     QuestionAnsweringData,
+    QuestionAnsweringTask,
     Seq2SeqData,
     Seq2SeqTask,
     SummarizationData,
