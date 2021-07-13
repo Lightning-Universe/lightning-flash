8833399ed28c1442110841e53daacae050914065
Akihiro Nitta <nitta@akihironitta.com>
2021-07-14 00:45:35 +0900

    Fix imports - text

--- flash/text/__init__.py
+++ flash/text/__init__.py
@@ -1,11 +1,19 @@
-from flash.text.classification import TextClassificationData, TextClassifier  # noqa: F401
-from flash.text.seq2seq import (  # noqa: F401
-    QuestionAnsweringData,
-    QuestionAnsweringTask,
-    Seq2SeqData,
-    Seq2SeqTask,
-    SummarizationData,
-    SummarizationTask,
-    TranslationData,
-    TranslationTask,
-)
+from flash.text.classification.data import TextClassificationData
+from flash.text.classification.model import TextClassifier
+from flash.text.seq2seq.question_answering.data import QuestionAnsweringData
+from flash.text.seq2seq.question_answering.model import QuestionAnsweringTask
+from flash.text.seq2seq.summarization.data import SummarizationData
+from flash.text.seq2seq.summarization.model import SummarizationTask
+from flash.text.seq2seq.translation.data import TranslationData
+from flash.text.seq2seq.translation.model import TranslationTask
+
+__all__ = [
+    "TextClassificationData",
+    "TextClassifier",
+    "QuestionAnsweringData",
+    "QuestionAnsweringTask",
+    "SummarizationData",
+    "SummarizationTask",
+    "TranslationData",
+    "TranslationTask",
+]
