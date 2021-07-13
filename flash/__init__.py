9b8b9a6245dbb206cdbef1bee75da386be017082
Akihiro Nitta <nitta@akihironitta.com>
2021-07-14 00:30:44 +0900

    Fix imports - flash

--- flash/__init__.py
+++ flash/__init__.py
@@ -11,37 +11,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Root package info."""
 import os
 
 from flash.__about__ import *  # noqa: F401 F403
-from flash.core.utilities.imports import _TORCH_AVAILABLE
+from flash.core.data.callback import FlashCallback
+from flash.core.data.data_module import DataModule
+from flash.core.data.data_source import DataSource
+from flash.core.data.process import Postprocess, Preprocess, Serializer
+from flash.core.model import Task
+from flash.core.trainer import Trainer
 
-if _TORCH_AVAILABLE:
+_PACKAGE_ROOT = os.path.dirname(__file__)
+ASSETS_ROOT = os.path.join(_PACKAGE_ROOT, "assets")
+PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
+_IS_TESTING = os.getenv("FLASH_TESTING", "0") == "1"
 
-    from flash.core.data.callback import FlashCallback
-    from flash.core.data.data_module import DataModule  # noqa: E402
-    from flash.core.data.data_source import DataSource
-    from flash.core.data.process import Postprocess, Preprocess, Serializer
-    from flash.core.model import Task  # noqa: E402
-    from flash.core.trainer import Trainer  # noqa: E402
+if _IS_TESTING:
+    from pytorch_lightning import seed_everything
+    seed_everything(42)
 
-    _PACKAGE_ROOT = os.path.dirname(__file__)
-    ASSETS_ROOT = os.path.join(_PACKAGE_ROOT, "assets")
-    PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
-    _IS_TESTING = os.getenv("FLASH_TESTING", "0") == "1"
-
-    if _IS_TESTING:
-        from pytorch_lightning import seed_everything
-        seed_everything(42)
-
-    __all__ = [
-        "DataSource",
-        "DataModule",
-        "FlashCallback",
-        "Preprocess",
-        "Postprocess",
-        "Serializer",
-        "Task",
-        "Trainer",
-    ]
+__all__ = [
+    "DataSource",
+    "DataModule",
+    "FlashCallback",
+    "Preprocess",
+    "Postprocess",
+    "Serializer",
+    "Task",
+    "Trainer",
+]
