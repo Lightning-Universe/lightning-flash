96e4e5e4067fdeaa6ff621beeca6d87dbb7da29a
Akihiro Nitta <nitta@akihironitta.com>
2021-07-14 00:29:48 +0900

    Fix imports - image

--- flash/image/__init__.py
+++ flash/image/__init__.py
@@ -1,14 +1,23 @@
-from flash.image.backbones import IMAGE_CLASSIFIER_BACKBONES, OBJ_DETECTION_BACKBONES  # noqa: F401
-from flash.image.classification import (  # noqa: F401
-    ImageClassificationData,
-    ImageClassificationPreprocess,
-    ImageClassifier,
-)
-from flash.image.detection import ObjectDetectionData, ObjectDetector  # noqa: F401
-from flash.image.embedding import ImageEmbedder  # noqa: F401
-from flash.image.segmentation import (  # noqa: F401
-    SemanticSegmentation,
-    SemanticSegmentationData,
-    SemanticSegmentationPreprocess,
-)
-from flash.image.style_transfer import StyleTransfer, StyleTransferData, StyleTransferPreprocess  # noqa: F401
+from flash.image.backbones import IMAGE_CLASSIFIER_BACKBONES, OBJ_DETECTION_BACKBONES
+from flash.image.classification.data import ImageClassificationData
+from flash.image.classification.model import ImageClassifier
+from flash.image.detection.data import ObjectDetectionData
+from flash.image.detection.model import ObjectDetector
+from flash.image.embedding.model import ImageEmbedder
+from flash.image.segmentation.data import SemanticSegmentationData
+from flash.image.segmentation.model import SemanticSegmentation
+from flash.image.style_transfer import StyleTransfer, StyleTransferData
+
+__all__ = [
+    "ImageClassificationData",
+    "ImageClassifier",
+    "IMAGE_CLASSIFIER_BACKBONES",
+    "ImageEmbedder",
+    "OBJ_DETECTION_BACKBONES",
+    "ObjectDetectionData",
+    "ObjectDetector",
+    "SemanticSegmentation",
+    "SemanticSegmentationData",
+    "StyleTransfer",
+    "StyleTransferData",
+]
