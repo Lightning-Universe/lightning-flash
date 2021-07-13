093c2f9ae165ea2119a31137bff563ed37271354
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-13 16:24:14 +0530

    remove lrsapp

--- flash/image/segmentation/backbones.py
+++ flash/image/segmentation/backbones.py
@@ -14,51 +14,12 @@
 from functools import partial
 
 from flash.core.registry import FlashRegistry
-from flash.core.utilities.imports import _SEGMENTATION_MODELS_AVAILABLE, _TORCHVISION_AVAILABLE
-from flash.image.backbones import catch_url_error
-
-if _TORCHVISION_AVAILABLE:
-    from torchvision.models import mobilenetv3, resnet
+from flash.core.utilities.imports import _SEGMENTATION_MODELS_AVAILABLE
 
 if _SEGMENTATION_MODELS_AVAILABLE:
     import segmentation_models_pytorch as smp
 
-MOBILENET_MODELS = ["mobilenet_v3_large"]
-RESNET_MODELS = ["resnet50", "resnet101"]
-
 SEMANTIC_SEGMENTATION_BACKBONES = FlashRegistry("backbones")
-
-if _TORCHVISION_AVAILABLE:
-
-    def _load_resnet(model_name: str, pretrained: bool = True):
-        backbone = resnet.__dict__[model_name](
-            pretrained=pretrained,
-            replace_stride_with_dilation=[False, True, True],
-        )
-        return backbone
-
-    for model_name in RESNET_MODELS:
-        SEMANTIC_SEGMENTATION_BACKBONES(
-            fn=catch_url_error(partial(_load_resnet, model_name)),
-            name=model_name,
-            namespace="image/segmentation",
-            package="torchvision",
-        )
-
-    def _load_mobilenetv3(model_name: str, pretrained: bool = True):
-        backbone = mobilenetv3.__dict__[model_name](
-            pretrained=pretrained,
-            _dilated=True,
-        )
-        return backbone
-
-    for model_name in MOBILENET_MODELS:
-        SEMANTIC_SEGMENTATION_BACKBONES(
-            fn=catch_url_error(partial(_load_mobilenetv3, model_name)),
-            name=model_name,
-            namespace="image/segmentation",
-            package="torchvision",
-        )
 
 if _SEGMENTATION_MODELS_AVAILABLE:
 
