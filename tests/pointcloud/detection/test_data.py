6c6f678feb58b0a3be3803dadb825c7f11ff5147
tchaton <thomas@grid.ai>
2021-07-16 17:55:41 +0200

    update

--- tests/pointcloud/detection/test_data.py
+++ tests/pointcloud/detection/test_data.py
@@ -28,13 +28,13 @@ if _POINTCLOUD_TESTING:
 
 
 @pytest.mark.skipif(not _POINTCLOUD_TESTING, reason="pointcloud libraries aren't installed")
-def test_pointcloud_segmentation_data(tmpdir):
+def test_pointcloud_object_detection_data(tmpdir):
 
     seed_everything(52)
 
-    download_data("https://pl-flash-data.s3.amazonaws.com/KITTI_micro.zip", tmpdir)
+    download_data("https://pl-flash-data.s3.amazonaws.com/KITTI_micro.zip", "data")
 
-    dm = PointCloudObjectDetectorData.from_folders(train_folder=join(tmpdir, "KITTI_micro", "Kitti", "train"), )
+    dm = PointCloudObjectDetectorData.from_folders(train_folder=join("data", "KITTI_micro", "Kitti", "train"), )
 
     class MockModel(PointCloudObjectDetector):
 
@@ -51,7 +51,7 @@ def test_pointcloud_segmentation_data(tmpdir):
     trainer = Trainer(max_epochs=1, limit_train_batches=1, limit_val_batches=0)
     trainer.fit(model, dm)
 
-    predict_path = join(tmpdir, "KITTI_micro", "Kitti", "predict")
+    predict_path = join("data", "KITTI_micro", "Kitti", "predict")
     model.eval()
 
     predictions = model.predict([join(predict_path, "scans/000000.bin")])
