ea4604ffafbdfa0a48cf231a4284bfeca76c91b8
Ethan Harris <ewah1g13@soton.ac.uk>
2021-07-16 21:58:17 +0100

    Fix docs build (#603)
    * Fix docs
    
    * Fixes
    
    * Fixes
    
    * Fixes

--- flash/pointcloud/__init__.py
+++ flash/pointcloud/__init__.py
@@ -1,4 +1,2 @@
-from flash.pointcloud.detection.data import PointCloudObjectDetectorData  # noqa: F401
-from flash.pointcloud.detection.model import PointCloudObjectDetector  # noqa: F401
-from flash.pointcloud.segmentation.data import PointCloudSegmentationData  # noqa: F401
-from flash.pointcloud.segmentation.model import PointCloudSegmentation  # noqa: F401
+from flash.pointcloud.detection import PointCloudObjectDetector, PointCloudObjectDetectorData  # noqa: F401
+from flash.pointcloud.segmentation import PointCloudSegmentation, PointCloudSegmentationData  # noqa: F401
