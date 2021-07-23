5ad2c7fff2287591f5cd7c2a336bca1320b6d99e
Ethan Harris <ethanwharris@gmail.com>
2021-07-23 11:40:01 +0100

    Try something

--- flash/pointcloud/segmentation/data.py
+++ flash/pointcloud/segmentation/data.py
@@ -1,9 +1,8 @@
 from typing import Any, Callable, Dict, Optional, Tuple
 
 from flash.core.data.data_module import DataModule
-from flash.core.data.data_pipeline import Deserializer
 from flash.core.data.data_source import DataSource, DefaultDataKeys, DefaultDataSources
-from flash.core.data.process import Preprocess
+from flash.core.data.process import Deserializer, Preprocess
 from flash.core.utilities.imports import requires_extras
 from flash.pointcloud.segmentation.open3d_ml.sequences_dataset import SequencesDataset
 
