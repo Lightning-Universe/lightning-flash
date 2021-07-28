90ed202d535501bc58103f35c65f8ed5281c8491
ananyahjha93 <ananya@pytorchlightning.ai>
2021-07-28 09:13:48 -0400

    flash/image/backbones/timm.py

--- flash/image/backbones.py
+++ flash/image/backbones.py
@@ -19,10 +19,7 @@ from functools import partial
 from typing import Tuple, Union
 
 from flash.core.registry import FlashRegistry
-from flash.core.utilities.imports import _TIMM_AVAILABLE, _TORCHVISION_AVAILABLE
-
-if _TIMM_AVAILABLE:
-    import timm
+from flash.core.utilities.imports import _TORCHVISION_AVAILABLE
 
 if _TORCHVISION_AVAILABLE:
     import torchvision
@@ -219,30 +216,4 @@ def _wide_resnet50_w4(pretrained: str, weights_paths: dict = RESNET50_W4_WEIGHTS
     return backbone, 8192
 
 
-# Paper: Emerging Properties in Self-Supervised Vision Transformers
-# https://arxiv.org/abs/2104.14294 from Mathilde Caron and al. (29 Apr 2021)
-# weights from https://github.com/facebookresearch/dino
-def dino_deits16(*_, **__):
-    backbone = torch.hub.load('facebookresearch/dino:main', 'dino_deits16')
-    return backbone, 384
 
-
-def dino_deits8(*_, **__):
-    backbone = torch.hub.load('facebookresearch/dino:main', 'dino_deits8')
-    return backbone, 384
-
-
-def dino_vitb16(*_, **__):
-    backbone = torch.hub.load('facebookresearch/dino:main', 'dino_vitb16')
-    return backbone, 768
-
-
-def dino_vitb8(*_, **__):
-    backbone = torch.hub.load('facebookresearch/dino:main', 'dino_vitb8')
-    return backbone, 768
-
-
-IMAGE_CLASSIFIER_BACKBONES(dino_deits16)
-IMAGE_CLASSIFIER_BACKBONES(dino_deits8)
-IMAGE_CLASSIFIER_BACKBONES(dino_vitb16)
-IMAGE_CLASSIFIER_BACKBONES(dino_vitb8)
