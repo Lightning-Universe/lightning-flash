6f099b5a2caf8f0dc15e4cf88263f43c0ba7494b
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-29 17:12:05 +0530

    fix

--- flash_examples/image_transforms.py
+++ flash_examples/image_transforms.py
@@ -1,16 +1,21 @@
 from torchvision import transforms as T
 
+from flash.core.data.data_source import DefaultDataKeys
+from flash.core.data.transforms import ApplyToKeys, merge_transforms
 from flash.core.data.utils import download_data
 from flash.image import ImageClassificationData
-from flash.image.classification.transforms import default_transforms, merge_transforms
+from flash.image.classification.transforms import default_transforms
 
 image_size = (256, 256)
 default_image_transforms = default_transforms(image_size)
 
-post_tensor_transform = T.Compose([
-    T.RandomHorizontalFlip(), T.ColorJitter(),
-    T.RandomAutocontrast(), T.RandomPerspective()
-])
+post_tensor_transform = ApplyToKeys(
+    DefaultDataKeys.INPUT,
+    T.Compose([T.RandomHorizontalFlip(),
+               T.ColorJitter(),
+               T.RandomAutocontrast(),
+               T.RandomPerspective()])
+)
 
 new_transforms = merge_transforms(default_image_transforms, {"post_tensor_transform": post_tensor_transform})
 
