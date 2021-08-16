2e3c8912ee98dfcc5659db446eeaf5b951be3273
Jirka Borovec <Borda@users.noreply.github.com>
2021-08-16 17:34:39 +0200

    examples: set gpus=device_count (#638)
    * , gpus=-1
    
    * torch.cuda.device_count()
    
    * .
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply suggestions from code review
    
    Co-authored-by: Ethan Harris <ewah1g13@soton.ac.uk>
    
    * .
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ethan Harris <ewah1g13@soton.ac.uk>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- flash_examples/graph_classification.py
+++ flash_examples/graph_classification.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import torch
+
 import flash
 from flash.core.utilities.imports import _TORCH_GEOMETRIC_AVAILABLE
 from flash.graph import GraphClassificationData, GraphClassifier
@@ -32,7 +34,7 @@ datamodule = GraphClassificationData.from_datasets(
 model = GraphClassifier(num_features=datamodule.num_features, num_classes=datamodule.num_classes)
 
 # 3. Create the trainer and fit the model
-trainer = flash.Trainer(max_epochs=3)
+trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count())
 trainer.fit(model, datamodule=datamodule)
 
 # 4. Classify some graphs!
