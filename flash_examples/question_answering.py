2fa8d5e5f06e8630d2818fe5ea3c0a5dcb1fb632
Karthik Rangasai <karthikrangasai@gmail.com>
2021-08-17 11:21:07 +0530

    Updating question answering example

--- flash_examples/question_answering.py
+++ flash_examples/question_answering.py
@@ -33,6 +33,7 @@ trainer.finetune(model, datamodule=datamodule)
 
 # 4. Summarize some text!
 predictions = model.predict({
+    "id": ["56ddde6b9a695914005b9629", "56ddde6b9a695914005b9628"],
     "context": [
         """
         The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) were the people who in the 10th
@@ -43,11 +44,20 @@ predictions = model.predict({
         descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct
         cultural and ethnic identity of the Normans emerged initially in the first half of the 10th
         century, and it continued to evolve over the succeeding centuries.
+        """, """
+        The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) were the people who in the 10th
+        and 11th centuries gave their name to Normandy, a region in France. They were descended from Norse
+        ("Norman" comes from "Norseman") raiders and pirates from Denmark, Iceland and Norway who, under
+        their leader Rollo, agreed to swear fealty to King Charles III of West Francia. Through generations
+        of assimilation and mixing with the native Frankish and Roman-Gaulish populations, their
+        descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct
+        cultural and ethnic identity of the Normans emerged initially in the first half of the 10th
+        century, and it continued to evolve over the succeeding centuries.
         """
     ],
-    "question": ["When were the Normans in Normandy?"]
+    "question": ["When were the Normans in Normandy?", "In what country is Normandy located?"]
 })
-print(predictions[0])
+print(predictions)
 
 # 5. Save the model!
 trainer.save_checkpoint("question_answering_on_sqaud_v2.pt")
