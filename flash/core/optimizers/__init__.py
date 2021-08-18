da40da73b1c23c57321f393f4896904c8fa26978
ananyahjha93 <ananya@pytorchlightning.ai>
2021-08-18 00:14:37 -0400

    added exports

---
+++ flash/core/optimizers/__init__.py
@@ -0,0 +1,6 @@
+from flash.core.optimizers.lars import LARS  # noqa: F401
+from flash.core.optimizers.lamb import LAMB  # noqa: F401
+from flash.core.optimizers.lr_scheduler import (  # noqa: F401
+    LinearWarmupCosineAnnealingLR,
+    linear_warmup_decay,
+)
