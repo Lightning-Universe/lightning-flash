fa118b4510094726659aab6fcb2eec85463126ee
ananyahjha93 <ananya@pytorchlightning.ai>
2021-08-26 13:40:24 -0400

    test

--- tests/core/integrations/vissl/transforms.py
+++ tests/core/integrations/vissl/transforms.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pytest
-import torch
 
 from flash.image import ImageClassificationData
 from flash.core.utilities.imports import _IMAGE_AVAILABLE
