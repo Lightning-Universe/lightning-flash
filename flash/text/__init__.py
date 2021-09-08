e647cb2260cf55febfcf33728579a4d2c8a6c7e7
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-08 12:19:56 -0700

    Update __init__.py

--- flash/text/__init__.py
+++ flash/text/__init__.py
@@ -1,8 +1,7 @@
 from flash.text.classification import TextClassificationData, TextClassifier  # noqa: F401
 from flash.text.generation import TextGenerationData, TextGeneration  # noqa: F401
+from flash.text.question_answering import QuestionAnsweringData, QuestionAnsweringTask  # noqa: F401
 from flash.text.seq2seq import (  # noqa: F401
-    QuestionAnsweringData,
-    QuestionAnsweringTask,
     Seq2SeqData,
     Seq2SeqTask,
     SummarizationData,
