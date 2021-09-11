ddd5b5b21459850af9e9840c4c2f1db369326071
Ananya Harsh Jha <ananya@pytorchlightning.ai>
2021-09-11 16:34:04 -0400

    strategies

--- flash/image/embedding/losses/vissl_losses.py
+++ flash/image/embedding/losses/vissl_losses.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List
+from typing import List, Union
 
 from flash.core.registry import FlashRegistry
 from flash.core.utilities.imports import _VISSL_AVAILABLE
@@ -48,9 +48,113 @@ def dino_loss(
             "output_dim": output_dim,
         }
     )
+
     loss_fn = LOSS_REGISTRY["dino_loss"](cfg)
     return loss_fn
 
 
+def swav_loss(
+    embedding_dim: int = 128,
+    temperature: float = 0.1,
+    use_double_precision: bool = False,
+    normalize_last_layer: bool = True,
+    num_iters: int = 3,
+    epsilon: float = 0.05,
+    num_crops: int = 8,
+    crops_for_assign: List[int] = [0, 1],
+    num_prototypes: Union[int, List[int]] = 3000,
+    temp_hard_assignment_iters: int = 0,
+    output_dir: str = ".",
+    queue_length: int = 0,
+    start_iter: int = 0,
+    local_queue_length: int = 0,
+):
+    cfg = AttrDict(
+        {
+            "embedding_dim": embedding_dim,
+            "temperature": temperature,
+            "use_double_precision": use_double_precision,
+            "normalize_last_layer": normalize_last_layer,
+            "num_iters": num_iters,
+            "epsilon": epsilon,
+            "num_crops": num_crops,
+            "crops_for_assign": crops_for_assign,
+            "num_prototypes": [num_prototypes] if isinstance(num_prototypes, int) else num_prototypes,
+            "temp_hard_assignment_iters": temp_hard_assignment_iters,
+            "output_dir": output_dir,
+            "queue": AttrDict(
+                {
+                    "queue_length": queue_length,
+                    "start_iter": start_iter,
+                    "local_queue_length": local_queue_length,
+                }
+            )
+        }
+    )
+
+    loss_fn = LOSS_REGISTRY["swav_loss"](cfg)
+    return loss_fn
+
+
+def barlow_twins_loss(
+    lambda_: float = 0.0051,
+    scale_loss: float = 0.024,
+    embedding_dim: int = 8192
+):
+    cfg = AttrDict(
+        {
+            "lambda_": lambda_,
+            "scale_loss": scale_loss,
+            "embedding_dim": embedding_dim,
+        }
+    )
+
+    loss_fn = LOSS_REGISTRY["barlow_twins_loss"](cfg)
+    return loss_fn
+
+
+def simclr_loss(
+    temperature: float = 0.1,
+    embedding_dim: int = 128,
+    effective_batch_size: int = -1,
+    world_size: int = -1,
+):
+    cfg = AttrDict(
+        {
+            "temperature": temperature,
+            "buffer_params": AttrDict(
+                {
+                    "world_size": world_size,
+                    "embedding_dim": embedding_dim,
+                    "effective_batch_size": effective_batch_size,
+                }
+            )
+        }
+    )
+
+    loss_fn = LOSS_REGISTRY["simclr_info_nce_loss"](cfg)
+    return loss_fn
+
+
+def moco_loss(
+    embedding_dim: int = 128,
+    queue_size: int = 65536,
+    momentum: float = 0.999,
+    temperature: int = 0.2,
+):
+    cfg = AttrDict(
+        {
+            "embedding_dim": embedding_dim,
+            "queue_size": queue_size,
+            "momentum": momentum,
+            "temperature": temperature,
+        }
+    )
+
+    loss_fn = LOSS_REGISTRY["moco_loss"](cfg)
+    return loss_fn
+
+
 def register_vissl_losses(register: FlashRegistry):
-    register(dino_loss, name="dino_loss")
+    for loss_fn in (dino_loss, swav_loss, barlow_twins_loss, simclr_loss, moco_loss):
+        register(loss_fn)
--- flash/image/embedding/strategies/vissl_strategies.py
+++ flash/image/embedding/strategies/vissl_strategies.py
@@ -17,10 +17,19 @@ from flash.core.utilities.providers import _VISSL
 
 if _VISSL_AVAILABLE:
     from vissl.hooks.dino_hooks import DINOHook
+    from vissl.hooks.moco_hooks import 
+    from vissl.hooks.swav
 
     from flash.image.embedding.heads import IMAGE_EMBEDDER_HEADS
     from flash.image.embedding.losses import IMAGE_EMBEDDER_LOSS_FUNCTIONS
     from flash.image.embedding.vissl.adapter import VISSLAdapter
+
+
+HOOKS_DICT = {
+    "dino": [DINOHook()],
+    "moco": [],
+    "swav": [],
+}
 
 
 def dino(head: str = "swav_head", **kwargs):
@@ -30,5 +39,39 @@ def dino(head: str = "swav_head", **kwargs):
     return loss_fn, head
 
 
+def swav(head: str = "swav_head", **kwargs):
+    loss_fn = IMAGE_EMBEDDER_LOSS_FUNCTIONS.get("swav_loss")(**kwargs)
+    head = IMAGE_EMBEDDER_HEADS.get(head)(**kwargs)
+
+    return loss_fn, head
+
+
+def simclr(head: str = "simclr_head", **kwargs):
+    loss_fn = IMAGE_EMBEDDER_LOSS_FUNCTIONS.get("simclr_loss")(**kwargs)
+    head = IMAGE_EMBEDDER_HEADS.get(head)(**kwargs)
+
+    return loss_fn, head
+
+
+def moco(head: str = "simclr_head", **kwargs):
+    loss_fn = IMAGE_EMBEDDER_LOSS_FUNCTIONS.get("moco_loss")(**kwargs)
+    head = IMAGE_EMBEDDER_HEADS.get(head)(**kwargs)
+
+    return loss_fn, head
+
+
+def barlow_twins(head: str = "barlow_twins_head", **kwargs):
+    loss_fn = IMAGE_EMBEDDER_LOSS_FUNCTIONS.get("barlow_twins_loss")(**kwargs)
+    head = IMAGE_EMBEDDER_HEADS.get(head)(**kwargs)
+
+    return loss_fn, head
+
+
 def register_vissl_strategies(register: FlashRegistry):
-    register(dino, name="dino", adapter=VISSLAdapter, hooks=[DINOHook()], providers=_VISSL)
+    for training_strategy in (dino, swav, simclr, moco, barlow_twins):
+        register(
+            training_strategy,
+            hooks=HOOKS_DICT[training_strategy.__name__],
+            adapter=VISSLAdapter,
+            providers=_VISSL
+        )
