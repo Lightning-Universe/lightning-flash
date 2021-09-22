469285b405ad7d20f607b6672d4cfa2d383b12df
ananyahjha93 <ananya@pytorchlightning.ai>
2021-09-22 11:49:29 -0400

    tests

--- tests/core/integrations/vissl/test_transforms.py
+++ tests/core/integrations/vissl/test_transforms.py
@@ -21,21 +21,21 @@ from tests.image.embedding.utils import ssl_datamodule
 @pytest.mark.skipif(not (_TORCHVISION_AVAILABLE and _VISSL_AVAILABLE), reason="vissl not installed.")
 def test_multicrop_input_transform():
     batch_size = 8
-    total_crops = 6
+    total_num_crops = 6
     num_crops = [2, 4]
     size_crops = [160, 96]
     crop_scales = [[0.4, 1], [0.05, 0.4]]
 
     train_dataloader = ssl_datamodule(
         batch_size=batch_size,
-        total_crops=total_crops,
+        total_num_crops=total_num_crops,
         num_crops=num_crops,
         size_crops=size_crops,
         crop_scales=crop_scales,
     )._train_dataloader()
     batch = next(iter(train_dataloader))
 
-    assert len(batch[DefaultDataKeys.INPUT]) == total_crops
+    assert len(batch[DefaultDataKeys.INPUT]) == total_num_crops
     assert batch[DefaultDataKeys.INPUT][0].shape == (batch_size, 3, size_crops[0], size_crops[0])
     assert batch[DefaultDataKeys.INPUT][-1].shape == (batch_size, 3, size_crops[-1], size_crops[-1])
     assert list(batch[DefaultDataKeys.TARGET].shape) == [batch_size]
