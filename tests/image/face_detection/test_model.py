021b12eff946904eacedc39ecd91d4bc038847e8
ananyahjha93 <ananya@pytorchlightning.ai>
2021-09-24 13:16:15 -0400

    more coverage

--- tests/image/face_detection/test_model.py
+++ tests/image/face_detection/test_model.py
@@ -14,14 +14,21 @@
 import pytest
 
 import flash
+from flash.core.registry import FlashRegistry
 from flash.core.utilities.imports import _FASTFACE_AVAILABLE
 from flash.image import FaceDetectionData, FaceDetector
 
 if _FASTFACE_AVAILABLE:
     import fastface as ff
 
+    from fastface.arch.lffd import LFFD
+    from flash.image.face_detection.backbones import FACE_DETECTION_BACKBONES
+else:
+    FACE_DETECTION_BACKBONES = FlashRegistry("face_detection_backbones")
+    LFFD = object
 
-@pytest.mark.skipif(not _FASTFACE_AVAILABLE, reason="vissl not installed.")
+
+@pytest.mark.skipif(not _FASTFACE_AVAILABLE, reason="fastface not installed.")
 def test_fastface_training():
     dataset = ff.dataset.FDDBDataset(source_dir="data/", phase="val")
     datamodule = FaceDetectionData.from_datasets(train_dataset=dataset, batch_size=2)
@@ -30,3 +37,13 @@ def test_fastface_training():
 
     trainer = flash.Trainer(max_steps=2, num_sanity_val_steps=0)
     trainer.finetune(model, datamodule=datamodule, strategy="freeze")
+
+
+@pytest.mark.skipif(not _FASTFACE_AVAILABLE, reason="fastface not installed.")
+def test_fastface_backbones_registry():
+    backbones = FACE_DETECTION_BACKBONES.available_keys()
+    assert 'lffd_slim' in backbones
+    assert 'lffd_original' in backbones
+
+    backbone = FACE_DETECTION_BACKBONES.get('lffd_original')
+    assert isinstance(backbone, LFFD)
