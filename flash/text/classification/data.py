0a286723468f946206f23c8912bff46f483e5361
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2021-09-29 00:19:34 +0300

    Adding integration with Label Studio (#554)
    Co-authored-by: thomas chaton <thomas@grid.ai>

--- flash/text/classification/data.py
+++ flash/text/classification/data.py
@@ -25,6 +25,7 @@ from flash.core.data.callback import BaseDataFetcher
 from flash.core.data.data_module import DataModule
 from flash.core.data.data_source import DataSource, DefaultDataSources, LabelsState
 from flash.core.data.process import Deserializer, Postprocess, Preprocess
+from flash.core.integrations.labelstudio.data_source import LabelStudioTextClassificationDataSource
 from flash.core.utilities.imports import _TEXT_AVAILABLE, requires
 
 if _TEXT_AVAILABLE:
@@ -331,6 +332,9 @@ class TextClassificationPreprocess(Preprocess):
                 DefaultDataSources.JSON: TextJSONDataSource(self.backbone, max_length=max_length),
                 "data_frame": TextDataFrameDataSource(self.backbone, max_length=max_length),
                 "sentences": TextSentencesDataSource(self.backbone, max_length=max_length),
+                DefaultDataSources.LABELSTUDIO: LabelStudioTextClassificationDataSource(
+                    backbone=self.backbone, max_length=max_length
+                ),
             },
             default_data_source="sentences",
             deserializer=TextDeserializer(backbone, max_length),
