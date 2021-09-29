0e4c86225b11e3cf7f3036dd028b70b04ae99bbe
ananyahjha93 <ananya@pytorchlightning.ai>
2021-09-28 21:30:42 -0400

    .

--- tests/core/test_utils.py
+++ tests/core/test_utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+
 import pytest
 
 from flash.core.data.utils import download_data
