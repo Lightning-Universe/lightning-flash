2b7c3260751079212c018b1e397fc4d8027c272e
ananyahjha93 <ananya@pytorchlightning.ai>
2021-09-28 21:01:34 -0400

    .

--- tests/core/test_utils.py
+++ tests/core/test_utils.py
@@ -55,3 +55,9 @@ def test_download_data(tmpdir):
     download_data("https://pl-flash-data.s3.amazonaws.com/titanic.zip", path)
     assert "titanic" in set(os.listdir(path))
     assert "titanic.zip" in set(os.listdir(path))
+
+def test_download_targz_data(tmpdir):
+    path = os.path.join(tmpdir, "data")
+    download_data("https://pl-flash-data.s3.amazonaws.com/titanic.tar.gz", path)
+    assert "titanic" in set(os.listdir(path))
+    assert "titanic.tar.gz" in set(os.listdir(path))
