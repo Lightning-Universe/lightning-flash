a94b10737110b46ea247f91cfa90a2618a1a0899
ananyahjha93 <ananya@pytorchlightning.ai>
2021-09-30 10:41:21 -0400

    .

--- tests/image/face_detection/test_model.py
+++ tests/image/face_detection/test_model.py
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pytest
-
 import torch
-import flash
 
+import flash
 from flash.core.registry import FlashRegistry
 from flash.core.utilities.imports import _FASTFACE_AVAILABLE
 from flash.image import FaceDetectionData, FaceDetector
