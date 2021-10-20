fccdc69ce31065aab0f545de83bdef8760fd7540
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2021-10-20 17:13:08 +0530

    cross_encoders

---
+++ flash/text/embeddings/cross_encoders/__init__.py
@@ -0,0 +1,1 @@
+from flash.text.embeddings.cross_encoders.model import CrossEncoder
--- flash/text/embeddings/cross_encoders/backbones.py
+++ flash/text/embeddings/cross_encoders/backbones.py
@@ -2,13 +2,13 @@ from flash.core.registry import ExternalRegistry, FlashRegistry
 from flash.core.utilities.imports import _TEXT_AVAILABLE
 from flash.core.utilities.providers import _SENTENCE_TRANSFORMERS
 
+
 SENTENCE_TRANSFORMERS_BACKBONE = FlashRegistry("backbones")
 
 if _TEXT_AVAILABLE:
-    from sentence_transformers import SentenceTransformer
-
+    from sentence_transformers.cross_encoder import CrossEncoder
     SENTENCE_TRANSFORMERS_BACKBONE += ExternalRegistry(
-        SentenceTransformer,
+        CrossEncoder,
         "backbones",
         _SENTENCE_TRANSFORMERS,
     )
