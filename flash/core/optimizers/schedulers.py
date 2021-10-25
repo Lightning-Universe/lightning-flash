2d9ce85ef1560c87824fbe6f9965239dd9509ce5
Ethan Harris <ethanwharris@gmail.com>
2021-10-25 12:57:40 +0100

    Debugging

--- flash/core/optimizers/schedulers.py
+++ flash/core/optimizers/schedulers.py
@@ -13,26 +13,26 @@ from torch.optim.lr_scheduler import (
 )
 
 from flash.core.registry import FlashRegistry
-from flash.core.utilities.imports import _TRANSFORMERS_AVAILABLE
+from flash.core.utilities.imports import _TORCH_AVAILABLE, _TRANSFORMERS_AVAILABLE
 from flash.core.utilities.providers import _HUGGINGFACE
 
 _SCHEDULERS_REGISTRY = FlashRegistry("scheduler")
 _STEP_SCHEDULERS = (StepLR, MultiStepLR, CosineAnnealingLR, CyclicLR, CosineAnnealingWarmRestarts)
 
-schedulers: List[_LRScheduler] = []
-for n in dir(lr_scheduler):
-    sched = getattr(lr_scheduler, n)
+if _TORCH_AVAILABLE:
+    schedulers: List[_LRScheduler] = []
+    for n in dir(lr_scheduler):
+        sched = getattr(lr_scheduler, n)
 
-    if inspect.isclass(sched) and sched != _LRScheduler and issubclass(sched, _LRScheduler):
-        schedulers.append(sched)
+        if inspect.isclass(sched) and sched != _LRScheduler and issubclass(sched, _LRScheduler):
+            schedulers.append(sched)
 
-# Adding `ReduceLROnPlateau` separately as it is subclassed from `object` and not `_LRScheduler`.
-schedulers.append(ReduceLROnPlateau)
+    # Adding `ReduceLROnPlateau` separately as it is subclassed from `object` and not `_LRScheduler`.
+    schedulers.append(ReduceLROnPlateau)
 
-
-for scheduler in schedulers:
-    interval = "step" if issubclass(scheduler, _STEP_SCHEDULERS) else "epoch"
-    _SCHEDULERS_REGISTRY(scheduler, name=scheduler.__name__.lower(), interval=interval)
+    for scheduler in schedulers:
+        interval = "step" if issubclass(scheduler, _STEP_SCHEDULERS) else "epoch"
+        _SCHEDULERS_REGISTRY(scheduler, name=scheduler.__name__.lower(), interval=interval)
 
 if _TRANSFORMERS_AVAILABLE:
     from transformers import optimization
