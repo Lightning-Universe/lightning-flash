48b3bdf21cfad468b69424a5f9f01a80c45f5ef0
Ethan Harris <ethanwharris@gmail.com>
2021-11-12 12:40:06 +0000

    Try fix

--- flash/core/data/io/input_base.py
+++ flash/core/data/io/input_base.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import abc
 import functools
+import sys
 from typing import Any, Iterable, MutableMapping, Sequence, Union
 
 from torch.utils.data import Dataset, IterableDataset
@@ -20,6 +20,11 @@ from torch.utils.data import Dataset, IterableDataset
 from flash.core.data.data_pipeline import DataPipeline
 from flash.core.data.properties import Properties
 from flash.core.utilities.stages import RunningStage
+
+if sys.version_info < (3, 7):
+    from typing import GenericMeta
+else:
+    GenericMeta = type
 
 
 def _has_len(data: Union[Sequence, Iterable]) -> bool:
@@ -51,7 +56,7 @@ def _wrap_init(dct):
         dct["__init__"] = wrapper
 
 
-class _InputMeta(abc.ABCMeta):
+class _InputMeta(GenericMeta):
     def __new__(mcs, name, bases, dct):
         _wrap_init(dct)
         return super().__new__(mcs, name, bases, dct)
