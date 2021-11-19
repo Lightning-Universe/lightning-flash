afb954cef783be7e0a4fe449e377e1c5894167ed
Ethan Harris <ethanwharris@gmail.com>
2021-11-19 11:31:40 +0000

    Improve CSV reading

--- flash/core/data/utilities/paths.py
+++ flash/core/data/utilities/paths.py
@@ -1,10 +1,28 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
+import warnings
 from typing import Any, Callable, cast, Dict, List, Optional, Tuple, Union
 
+import pandas as pd
 
-# Copied from torchvision:
+from flash.core.utilities.types import PATH_TYPE
+
+
+# adapted from torchvision:
 # https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py#L10
-def has_file_allowed_extension(filename: str, extensions: Tuple[str, ...]) -> bool:
+def has_file_allowed_extension(filename: PATH_TYPE, extensions: Tuple[str, ...]) -> bool:
     """Checks if a file is an allowed extension.
 
     Args:
@@ -14,13 +32,13 @@ def has_file_allowed_extension(filename: str, extensions: Tuple[str, ...]) -> bo
     Returns:
         bool: True if the filename ends with one of given extensions
     """
-    return filename.lower().endswith(extensions)
+    return str(filename).lower().endswith(extensions)
 
 
 # Copied from torchvision:
 # https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py#L48
 def make_dataset(
-    directory: str,
+    directory: PATH_TYPE,
     class_to_idx: Dict[str, int],
     extensions: Optional[Tuple[str, ...]] = None,
     is_valid_file: Optional[Callable[[str], bool]] = None,
@@ -44,7 +62,7 @@ def make_dataset(
         List[Tuple[str, int]]: samples of a form (path_to_sample, class)
     """
     instances = []
-    directory = os.path.expanduser(directory)
+    directory = os.path.expanduser(str(directory))
     both_none = extensions is None and is_valid_file is None
     both_something = extensions is not None and is_valid_file is not None
     if both_none or both_something:
@@ -77,7 +95,7 @@ def isdir(path: Any) -> bool:
         return False
 
 
-def find_classes(dir: str) -> Tuple[List[str], Dict[str, int]]:
+def find_classes(dir: PATH_TYPE) -> Tuple[List[str], Dict[str, int]]:
     """Finds the class folders in a dataset. Ensures that no class is a subdirectory of another.
 
     Args:
@@ -86,13 +104,15 @@ def find_classes(dir: str) -> Tuple[List[str], Dict[str, int]]:
     Returns:
         (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
     """
-    classes = [d.name for d in os.scandir(dir) if d.is_dir()]
+    classes = [d.name for d in os.scandir(str(dir)) if d.is_dir()]
     classes.sort()
     class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
     return classes, class_to_idx
 
 
-def list_valid_files(paths: Union[str, List[str]], valid_extensions: Optional[Tuple[str, ...]] = None) -> List[str]:
+def list_valid_files(
+    paths: Union[PATH_TYPE, List[PATH_TYPE]], valid_extensions: Optional[Tuple[str, ...]] = None
+) -> List[PATH_TYPE]:
     """List the files with a valid extension present in: a single file, a list of files, or a directory.
 
     Args:
@@ -116,3 +136,19 @@ def list_valid_files(paths: Union[str, List[str]], valid_extensions: Optional[Tu
             paths,
         )
     )
+
+
+def read_csv(file: PATH_TYPE) -> pd.DataFrame:
+    """A wrapper for ``pd.read_csv`` which tries to handle errors gracefully.
+
+    Args:
+        file: The CSV file to read.
+
+    Returns:
+        A ``DataFrame`` containing the contents of the file.
+    """
+    try:
+        return pd.read_csv(file, encoding="utf-8")
+    except UnicodeDecodeError:
+        warnings.warn("A UnicodeDecodeError was raised when reading the CSV. This error will be ignored.")
+        return pd.read_csv(file, encoding="utf-8", encoding_errors="ignore")
--- flash/core/utilities/types.py
+++ flash/core/utilities/types.py
@@ -1,3 +1,17 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
 from typing import Any, Callable, Dict, Mapping, Optional, Sequence, Tuple, Union
 
 from torch import nn
@@ -8,6 +22,7 @@ from flash.core.data.io.output import Output
 from flash.core.data.io.output_transform import OutputTransform
 from flash.core.data.process import Deserializer
 
+# Task Arguments
 MODEL_TYPE = Optional[nn.Module]
 LOSS_FN_TYPE = Optional[Union[Callable, Mapping, Sequence]]
 OPTIMIZER_TYPE = Union[str, Callable, Tuple[str, Dict[str, Any]]]
@@ -15,7 +30,12 @@ LR_SCHEDULER_TYPE = Optional[
     Union[str, Callable, Tuple[str, Dict[str, Any]], Tuple[str, Dict[str, Any], Dict[str, Any]]]
 ]
 METRICS_TYPE = Union[Metric, Mapping, Sequence, None]
+
+# Data Pipeline
 DESERIALIZER_TYPE = Optional[Union[Deserializer, Mapping[str, Deserializer]]]
 INPUT_TRANSFORM_TYPE = Optional[InputTransform]
 OUTPUT_TRANSFORM_TYPE = Optional[OutputTransform]
 OUTPUT_TYPE = Optional[Output]
+
+# Utilities
+PATH_TYPE = Union[str, bytes, os.PathLike]
