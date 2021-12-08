b9275dfe5fe32246d8f35800f9be509917560694
Ethan Harris <ethanwharris@gmail.com>
2021-12-08 14:44:26 +0000

    Fixes

--- flash/core/utilities/types.py
+++ flash/core/utilities/types.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Callable, Dict, Mapping, Optional, Sequence, Tuple, Type, TypeVar, Union
+from typing import Any, Callable, Dict, Mapping, Sequence, Tuple, Type, TypeVar, Union
 
 from pytorch_lightning.utilities.enums import LightningEnum
 from torch import nn
@@ -20,13 +20,13 @@ from torchmetrics import Metric
 import flash
 
 # Task Arguments
-MODEL_TYPE = Optional[nn.Module]
-LOSS_FN_TYPE = Optional[Union[Callable, Mapping, Sequence]]
-OPTIMIZER_TYPE = Union[str, Callable, Tuple[str, Dict[str, Any]]]
-LR_SCHEDULER_TYPE = Optional[
-    Union[str, Callable, Tuple[str, Dict[str, Any]], Tuple[str, Dict[str, Any], Dict[str, Any]]]
-]
-METRICS_TYPE = Union[Metric, Mapping, Sequence, None]
+MODEL_TYPE = TypeVar("MODEL_TYPE", nn.Module, None)
+LOSS_FN_TYPE = TypeVar("LOSS_FN_TYPE", Callable, Mapping, Sequence, None)
+OPTIMIZER_TYPE = TypeVar("OPTIMIZER_TYPE", str, Callable, Tuple[str, Dict[str, Any]], None)
+LR_SCHEDULER_TYPE = TypeVar(
+    "LR_SCHEDULER_TYPE", str, Callable, Tuple[str, Dict[str, Any]], Tuple[str, Dict[str, Any], Dict[str, Any]], None
+)
+METRICS_TYPE = TypeVar("METRICS_TYPE", Metric, Mapping, Sequence, None)
 
 # Data Pipeline
 DESERIALIZER_TYPE = TypeVar("DESERIALIZER_TYPE", "flash.core.data.process.Deserializer", None)
