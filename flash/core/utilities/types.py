5f53e7e5b3f633a4e1d49daa58996814627f77a7
Ethan Harris <ewah1g13@soton.ac.uk>
2021-12-08 15:08:08 +0000

    Fix typing for RTD build (#1035)

--- flash/core/utilities/types.py
+++ flash/core/utilities/types.py
@@ -11,36 +11,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Callable, Dict, Mapping, Optional, Sequence, Tuple, Type, Union
+from typing import Any, Callable, Dict, Mapping, Sequence, Tuple, Type, TypeVar, Union
 
 from pytorch_lightning.utilities.enums import LightningEnum
 from torch import nn
 from torchmetrics import Metric
 
 import flash
-from flash.core.data.io.output import Output
 
 # Task Arguments
-MODEL_TYPE = Optional[nn.Module]
-LOSS_FN_TYPE = Optional[Union[Callable, Mapping, Sequence]]
-OPTIMIZER_TYPE = Union[str, Callable, Tuple[str, Dict[str, Any]]]
-LR_SCHEDULER_TYPE = Optional[
-    Union[str, Callable, Tuple[str, Dict[str, Any]], Tuple[str, Dict[str, Any], Dict[str, Any]]]
-]
-METRICS_TYPE = Union[Metric, Mapping, Sequence, None]
+MODEL_TYPE = TypeVar("MODEL_TYPE", nn.Module, None)
+LOSS_FN_TYPE = TypeVar("LOSS_FN_TYPE", Callable, Mapping, Sequence, None)
+OPTIMIZER_TYPE = TypeVar("OPTIMIZER_TYPE", str, Callable, Tuple[str, Dict[str, Any]], None)
+LR_SCHEDULER_TYPE = TypeVar(
+    "LR_SCHEDULER_TYPE", str, Callable, Tuple[str, Dict[str, Any]], Tuple[str, Dict[str, Any], Dict[str, Any]], None
+)
+METRICS_TYPE = TypeVar("METRICS_TYPE", Metric, Mapping, Sequence, None)
 
 # Data Pipeline
-DESERIALIZER_TYPE = Optional[
-    Union["flash.core.data.process.Deserializer", Mapping[str, "flash.core.data.process.Deserializer"]]
-]
-INPUT_TRANSFORM_TYPE = Optional[
-    Union[
-        Type["flash.InputTransform"],
-        Callable,
-        Tuple[Union[LightningEnum, str], Dict[str, Any]],
-        Union[LightningEnum, str],
-    ]
-]
-
-OUTPUT_TRANSFORM_TYPE = Optional["flash.OutputTransform"]
-OUTPUT_TYPE = Optional[Output]
+DESERIALIZER_TYPE = TypeVar("DESERIALIZER_TYPE", "flash.core.data.process.Deserializer", None)
+INPUT_TRANSFORM_TYPE = TypeVar(
+    "INPUT_TRANSFORM_TYPE",
+    Type["flash.core.data.input_transform.InputTransform"],
+    Callable,
+    Tuple[Union[LightningEnum, str], Dict[str, Any]],
+    Union[LightningEnum, str],
+    None,
+)
+OUTPUT_TRANSFORM_TYPE = TypeVar("OUTPUT_TRANSFORM_TYPE", "flash.core.data.io.output_transform.OutputTransform", None)
+OUTPUT_TYPE = TypeVar("OUTPUT_TYPE", "flash.core.data.io.output.Output", None)
