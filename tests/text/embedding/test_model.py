2071c9f0f6bcc1e68c242ab04107f291cb92d22b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2021-12-09 00:32:34 +0530

    Create test_model.py

---
+++ tests/text/embedding/test_model.py
@@ -0,0 +1,47 @@
+
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+
+import pytest
+import torch
+
+
+import flash
+from flash.text import SentenceEmbedder, TextClassificationData
+from tests.helpers.utils import  _TEXT_TESTING
+
+# ======== Mock functions ========
+
+datamodule = TextClassificationData.from_lists(
+      predict_data=[
+          "Turgid dialogue, feeble characterization - Harvey Keitel a judge?.",
+          "The worst movie in the history of cinema.",
+          "I come from Bulgaria where it 's almost impossible to have a tornado.",
+      ]
+  )
+
+# ==============================
+
+TEST_BACKBONE = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"  # super small model for testing
+model = SentenceEmbedder(backbone=TEST_BACKBONE)
+
+@pytest.mark.skipif(os.name == "nt", reason="Huggingface timing out on Windows")
+@pytest.mark.skipif(not _TEXT_TESTING, reason="text libraries aren't installed.")
+def test_predict(tmpdir):
+    trainer = flash.Trainer(gpus=torch.cuda.device_count())
+    predictions = trainer.predict(model, datamodule=datamodule)
+    assert[t.size() for t in predictions[0]]==[torch.Size([384]), torch.Size([384]), torch.Size([384])]
+
+
