9b5ab63dccba7a852f25316dce6beea77728366d
tchaton <thomas@grid.ai>
2021-12-09 11:53:24 +0000

    update

--- flash/image/keypoint_detection/data.py
+++ flash/image/keypoint_detection/data.py
@@ -11,64 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from functools import partial
-from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
+from typing import Any, Dict, List, Optional, Type
 
-from flash.core.data.data_module import DataModule
 from flash.core.data.data_pipeline import DataPipelineState
-from flash.core.data.io.input import InputFormat
-from flash.core.data.io.input_transform import InputTransform
+from flash.core.data.io.input_base import Input
+from flash.core.data.new_data_module import DataModule
 from flash.core.integrations.icevision.data import IceVisionInput
-from flash.core.integrations.icevision.transforms import default_transforms
+from flash.core.integrations.icevision.transforms import IceVisionInputTransform as KeypointDetectionInputTransform
 from flash.core.utilities.imports import _ICEVISION_AVAILABLE
 from flash.core.utilities.stages import RunningStage
+from flash.core.utilities.types import INPUT_TRANSFORM_TYPE
 
 if _ICEVISION_AVAILABLE:
     from icevision.parsers import COCOKeyPointsParser, Parser
 else:
     COCOKeyPointsParser = object
     Parser = object
-
-
-class KeypointDetectionInputTransform(InputTransform):
-    def __init__(
-        self,
-        train_transform: Optional[Dict[str, Callable]] = None,
-        val_transform: Optional[Dict[str, Callable]] = None,
-        test_transform: Optional[Dict[str, Callable]] = None,
-        predict_transform: Optional[Dict[str, Callable]] = None,
-        image_size: Tuple[int, int] = (128, 128),
-        parser: Optional[Callable] = None,
-    ):
-        self.image_size = image_size
-
-        super().__init__(
-            train_transform=train_transform,
-            val_transform=val_transform,
-            test_transform=test_transform,
-            predict_transform=predict_transform,
-            inputs={
-                "coco": partial(IceVisionInput, parser=COCOKeyPointsParser),
-                InputFormat.FILES: IceVisionInput,
-                InputFormat.FOLDERS: partial(IceVisionInput, parser=parser),
-            },
-            default_input=InputFormat.FILES,
-        )
-
-        self._default_collate = self._identity
-
-    def get_state_dict(self) -> Dict[str, Any]:
-        return {**self.transforms}
-
-    @classmethod
-    def load_state_dict(cls, state_dict: Dict[str, Any], strict: bool = False):
-        return cls(**state_dict)
-
-    def default_transforms(self) -> Optional[Dict[str, Callable]]:
-        return default_transforms(self.image_size)
-
-    def train_default_transforms(self) -> Optional[Dict[str, Callable]]:
-        return default_transforms(self.image_size)
 
 
 class KeypointDetectionData(DataModule):
@@ -85,29 +43,23 @@ class KeypointDetectionData(DataModule):
         test_folder: Optional[str] = None,
         test_ann_file: Optional[str] = None,
         predict_folder: Optional[str] = None,
-        train_transform: Optional[Dict[str, Callable]] = None,
-        val_transform: Optional[Dict[str, Callable]] = None,
-        test_transform: Optional[Dict[str, Callable]] = None,
-        predict_transform: Optional[Dict[str, Callable]] = None,
-        image_size: Tuple[int, int] = (128, 128),
-        parser: Optional[Union[Callable, Type[Parser]]] = None,
-        **data_module_kwargs,
+        train_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        val_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        test_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        predict_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        parser: Optional[Type[Parser]] = Parser,
+        input_cls: Type[Input] = IceVisionInput,
+        transform_kwargs: Optional[Dict] = None,
+        **data_module_kwargs: Any,
     ) -> "KeypointDetectionData":
 
-        dataset_kwargs = dict(parser=parser, data_pipeline_state=DataPipelineState())
+        ds_kw = dict(parser=parser, data_pipeline_state=DataPipelineState(), transform_kwargs=transform_kwargs)
 
         return cls(
-            IceVisionInput(RunningStage.TRAINING, train_folder, train_ann_file, **dataset_kwargs),
-            IceVisionInput(RunningStage.VALIDATING, val_folder, val_ann_file, **dataset_kwargs),
-            IceVisionInput(RunningStage.TESTING, test_folder, test_ann_file, **dataset_kwargs),
-            IceVisionInput(RunningStage.PREDICTING, predict_folder, **dataset_kwargs),
-            input_transform=cls.input_transform_cls(
-                train_transform,
-                val_transform,
-                test_transform,
-                predict_transform,
-                image_size=image_size,
-            ),
+            input_cls(RunningStage.TRAINING, train_folder, train_ann_file, transform=train_transform, **ds_kw),
+            input_cls(RunningStage.VALIDATING, val_folder, val_ann_file, transform=val_transform, **ds_kw),
+            input_cls(RunningStage.TESTING, test_folder, test_ann_file, transform=test_transform, **ds_kw),
+            input_cls(RunningStage.PREDICTING, predict_folder, transform=predict_transform, **ds_kw),
             **data_module_kwargs,
         )
 
@@ -121,12 +73,14 @@ class KeypointDetectionData(DataModule):
         test_folder: Optional[str] = None,
         test_ann_file: Optional[str] = None,
         predict_folder: Optional[str] = None,
-        train_transform: Optional[Dict[str, Callable]] = None,
-        val_transform: Optional[Dict[str, Callable]] = None,
-        test_transform: Optional[Dict[str, Callable]] = None,
-        predict_transform: Optional[Dict[str, Callable]] = None,
-        image_size: Tuple[int, int] = (128, 128),
-        **data_module_kwargs,
+        train_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        val_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        test_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        predict_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        parser: Optional[Type[Parser]] = COCOKeyPointsParser,
+        input_cls: Type[Input] = IceVisionInput,
+        transform_kwargs: Optional[Dict] = None,
+        **data_module_kwargs: Any,
     ):
         """Creates a :class:`~flash.image.keypoint_detection.data.KeypointDetectionData` object from the given data
         folders and annotation files in the COCO format.
@@ -147,22 +101,17 @@ class KeypointDetectionData(DataModule):
                 :class:`~flash.core.data.io.input_transform.InputTransform` hook names to callable transforms.
             predict_transform: The dictionary of transforms to use during predicting which maps
                 :class:`~flash.core.data.io.input_transform.InputTransform` hook names to callable transforms.
-            image_size: The size to resize images (and rescale their keypoints) to.
+            input_cls: The :class:`~flash.core.data.io.input_base.Input` used to create the dataset.
+            transform_kwargs: Keyword arguments provided to the transform on instantiation.
+            data_module_kwargs: Keyword arguments provided to the DataModule on instantiation.
         """
-        return cls.from_icedata(
-            train_folder=train_folder,
-            train_ann_file=train_ann_file,
-            val_folder=val_folder,
-            val_ann_file=val_ann_file,
-            test_folder=test_folder,
-            test_ann_file=test_ann_file,
-            predict_folder=predict_folder,
-            train_transform=train_transform,
-            val_transform=val_transform,
-            test_transform=test_transform,
-            predict_transform=predict_transform,
-            image_size=image_size,
-            parser=COCOKeyPointsParser,
+        ds_kw = dict(parser=parser, data_pipeline_state=DataPipelineState(), transform_kwargs=transform_kwargs)
+
+        return cls(
+            input_cls(RunningStage.TRAINING, train_folder, train_ann_file, transform=train_transform, **ds_kw),
+            input_cls(RunningStage.VALIDATING, val_folder, val_ann_file, transform=val_transform, **ds_kw),
+            input_cls(RunningStage.TESTING, test_folder, test_ann_file, transform=test_transform, **ds_kw),
+            input_cls(RunningStage.PREDICTING, predict_folder, transform=predict_transform, **ds_kw),
             **data_module_kwargs,
         )
 
@@ -170,8 +119,9 @@ class KeypointDetectionData(DataModule):
     def from_folders(
         cls,
         predict_folder: Optional[str] = None,
-        predict_transform: Optional[Dict[str, Callable]] = None,
-        image_size: Tuple[int, int] = (128, 128),
+        predict_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        input_cls: Type[Input] = IceVisionInput,
+        transform_kwargs: Optional[Dict] = None,
         **data_module_kwargs: Any,
     ) -> "DataModule":
         """Creates a :class:`~flash.core.data.data_module.DataModule` object from the given folders.
@@ -181,17 +131,17 @@ class KeypointDetectionData(DataModule):
         Args:
             predict_folder: The folder containing the predict data.
             predict_transform: The dictionary of transforms to use during predicting which maps
+            input_cls: The :class:`~flash.core.data.io.input_base.Input` used to create the dataset.
+            transform_kwargs: Keyword arguments provided to the transform on instantiation.
             data_module_kwargs: The keywords arguments for creating the datamodule.
 
         Returns:
             The constructed data module.
         """
+        ds_kw = dict(transform=predict_transform, transform_kwargs=transform_kwargs)
+
         return cls(
-            predict_dataset=IceVisionInput(RunningStage.PREDICTING, predict_folder),
-            input_transform=cls.input_transform_cls(
-                predict_transform=predict_transform,
-                image_size=image_size,
-            ),
+            predict_input=input_cls(RunningStage.PREDICTING, predict_folder, **ds_kw),
             **data_module_kwargs,
         )
 
@@ -199,8 +149,9 @@ class KeypointDetectionData(DataModule):
     def from_files(
         cls,
         predict_files: Optional[List[str]] = None,
-        predict_transform: Optional[Dict[str, Callable]] = None,
-        image_size: Tuple[int, int] = (128, 128),
+        predict_transform: INPUT_TRANSFORM_TYPE = KeypointDetectionInputTransform,
+        input_cls: Type[Input] = IceVisionInput,
+        transform_kwargs: Optional[Dict] = None,
         **data_module_kwargs: Any,
     ) -> "DataModule":
         """Creates a :class:`~flash.core.data.data_module.DataModule` object from the given a list of files.
@@ -209,18 +160,18 @@ class KeypointDetectionData(DataModule):
 
         Args:
             predict_files: The list of files containing the predict data.
-            predict_transform: The dictionary of transforms to use during predicting which maps
+            predict_transform: The dictionary of transforms to use during predicting which maps.
+            input_cls: The :class:`~flash.core.data.io.input_base.Input` used to create the dataset.
+            transform_kwargs: Keyword arguments provided to the transform on instantiation.
             data_module_kwargs: The keywords arguments for creating the datamodule.
 
         Returns:
             The constructed data module.
         """
+        ds_kw = dict(transform=predict_transform, transform_kwargs=transform_kwargs)
+
         return cls(
-            predict_dataset=IceVisionInput(RunningStage.PREDICTING, predict_files),
-            input_transform=cls.input_transform_cls(
-                predict_transform=predict_transform,
-                image_size=image_size,
-            ),
+            predict_input=input_cls(RunningStage.PREDICTING, predict_files, **ds_kw),
             **data_module_kwargs,
         )
 
