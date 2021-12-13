f75cc861c6534116ff60c43de886579612065728
Ethan Harris <ethanwharris@gmail.com>
2021-12-13 11:19:47 +0000

    Fix speech recognition test

--- tests/audio/speech_recognition/test_model.py
+++ tests/audio/speech_recognition/test_model.py
@@ -22,9 +22,9 @@ import torch
 from flash import Trainer
 from flash.__main__ import main
 from flash.audio import SpeechRecognition
-from flash.audio.speech_recognition.data import InputTransform, SpeechRecognitionOutputTransform
+from flash.audio.speech_recognition.data import InputTransform, SpeechRecognitionData, SpeechRecognitionOutputTransform
 from flash.audio.speech_recognition.input import SpeechRecognitionDeserializer
-from flash.core.data.io.input import DataKeys
+from flash.core.data.io.input import DataKeys, Input
 from flash.core.utilities.imports import _AUDIO_AVAILABLE
 from flash.core.utilities.stages import RunningStage
 from tests.helpers.utils import _AUDIO_TESTING, _SERVE_TESTING
@@ -53,9 +53,11 @@ TEST_BACKBONE = "patrickvonplaten/wav2vec2_tiny_random_robust"  # super small mo
 @pytest.mark.skipif(not _AUDIO_TESTING, reason="audio libraries aren't installed.")
 def test_init_train(tmpdir):
     model = SpeechRecognition(backbone=TEST_BACKBONE)
-    train_dl = torch.utils.data.DataLoader(DummyDataset())
+    datamodule = SpeechRecognitionData(
+        Input(RunningStage.TRAINING, DummyDataset(), transform=InputTransform), batch_size=2
+    )
     trainer = Trainer(default_root_dir=tmpdir, fast_dev_run=True)
-    trainer.fit(model, train_dl)
+    trainer.fit(model, datamodule=datamodule)
 
 
 @pytest.mark.skipif(not _AUDIO_TESTING, reason="audio libraries aren't installed.")
