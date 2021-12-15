6aa2267a9e365c4f2be75efd4c2098ccb82f1b25
Ethan Harris <ethanwharris@gmail.com>
2021-12-15 20:17:30 +0000

    Fix

--- tests/image/instance_segmentation/test_model.py
+++ tests/image/instance_segmentation/test_model.py
@@ -80,7 +80,7 @@ def test_instance_segmentation_inference(tmpdir):
     )
     predictions = trainer.predict(model, datamodule=datamodule)
     assert len(predictions[0]) == 3
-    assert len(predictions[0][0]) == 6
+    assert len(list(predictions[0][0].keys())) == 5
 
     model_path = os.path.join(tmpdir, "model.pt")
     trainer.save_checkpoint(model_path)
@@ -96,4 +96,4 @@ def test_instance_segmentation_inference(tmpdir):
     )
     predictions = trainer.predict(model, datamodule=datamodule)
     assert len(predictions[0]) == 3
-    assert len(predictions[0][0]) == 6
+    assert len(list(predictions[0][0].keys())) == 5
