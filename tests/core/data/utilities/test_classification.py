985fa831c3740291e2a3cb3cf92b6d0f7224a2f6
Ethan Harris <ethanwharris@gmail.com>
2021-12-15 11:48:53 +0000

    Add support for comma delimited multi-label targets

--- tests/core/data/utilities/test_classification.py
+++ tests/core/data/utilities/test_classification.py
@@ -50,6 +50,13 @@ cases = [
         ["blue", "green", "red"],
         3,
     ),
+    Case(
+        ["blue green", "green red", "red blue"],
+        [[1, 1, 0], [0, 1, 1], [1, 0, 1]],
+        TargetMode.MUTLI_SPACE_DELIMITED,
+        ["blue", "green", "red"],
+        3,
+    ),
     # Ambiguous
     Case([[0], [1, 2], [2, 0]], [[1, 0, 0], [0, 1, 1], [1, 0, 1]], TargetMode.MULTI_NUMERIC, None, 3),
     Case([[1, 0, 0], [0, 1, 1], [1, 0, 1]], [[1, 0, 0], [0, 1, 1], [1, 0, 1]], TargetMode.MULTI_BINARY, None, 3),
@@ -67,10 +74,24 @@ cases = [
         ["blue", "green", "red"],
         3,
     ),
+    Case(
+        ["blue", "green red", "red blue"],
+        [[1, 0, 0], [0, 1, 1], [1, 0, 1]],
+        TargetMode.MUTLI_SPACE_DELIMITED,
+        ["blue", "green", "red"],
+        3,
+    ),
     # Special cases
     Case(["blue ", " green", "red"], [0, 1, 2], TargetMode.SINGLE_TOKEN, ["blue", "green", "red"], 3),
     Case(
         ["blue", "green, red", "red, blue"],
+        [[1, 0, 0], [0, 1, 1], [1, 0, 1]],
+        TargetMode.MUTLI_COMMA_DELIMITED,
+        ["blue", "green", "red"],
+        3,
+    ),
+    Case(
+        ["blue", "green ,red", "red ,blue"],
         [[1, 0, 0], [0, 1, 1], [1, 0, 1]],
         TargetMode.MUTLI_COMMA_DELIMITED,
         ["blue", "green", "red"],
