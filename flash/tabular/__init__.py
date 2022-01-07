3b2d86819bde6c15ef3ca6d2d6f06c487812a07a
Ethan Harris <ewah1g13@soton.ac.uk>
2022-01-07 17:24:06 +0000

    Docstrings for `TabularForecastingData` (#1105)

--- flash/tabular/__init__.py
+++ flash/tabular/__init__.py
@@ -1,4 +1,5 @@
 from flash.tabular.classification import TabularClassificationData, TabularClassifier  # noqa: F401
 from flash.tabular.data import TabularData  # noqa: F401
 from flash.tabular.forecasting.data import TabularForecastingData  # noqa: F401
+from flash.tabular.forecasting.model import TabularForecaster  # noqa: F401
 from flash.tabular.regression import TabularRegressionData, TabularRegressor  # noqa: F401
