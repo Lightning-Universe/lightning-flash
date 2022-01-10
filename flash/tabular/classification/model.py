f141a3f8a7e1c03260404602aac7a1fc4b3b657f
Ethan Harris <ethanwharris@gmail.com>
2022-01-10 20:23:34 +0000

    Fix azure CI

--- flash/tabular/classification/model.py
+++ flash/tabular/classification/model.py
@@ -93,7 +93,7 @@ class TabularClassifier(ClassificationAdapterTask):
     @staticmethod
     def _ci_benchmark_fn(history: List[Dict[str, Any]]):
         """This function is used only for debugging usage with CI."""
-        assert history[-1]["val_accuracy"] > 0.6, history[-1]["val_accuracy"]
+        assert history[-1]["valid_accuracy"] > 0.6, history[-1]["valid_accuracy"]
 
     @classmethod
     def from_data(cls, datamodule, **kwargs) -> "TabularClassifier":
