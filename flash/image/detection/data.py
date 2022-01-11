3bb7b22ffd71c6f11bddc3306847f9daf0249b9b
Ethan Harris <ewah1g13@soton.ac.uk>
2022-01-11 18:06:48 +0000

    Update flash/image/detection/data.py
    Co-authored-by: Jirka Borovec <Borda@users.noreply.github.com>

--- flash/image/detection/data.py
+++ flash/image/detection/data.py
@@ -489,25 +489,13 @@ class ObjectDetectionData(DataModule):
             >>> _ = [rand_image.save(os.path.join("train_folder", f"image_{i}.png")) for i in range(1, 4)]
             >>> _ = [rand_image.save(os.path.join("predict_folder", f"predict_image_{i}.png")) for i in range(1, 4)]
             >>> annotations = {
-            ...     "image_1.png": {
-            ...         "filename": "image_1.png",
+            ...     f"image_{i+1}.png": {
+            ...         "filename": f"image_{i+1}.png",
             ...         "regions": [{
             ...                 "shape_attributes": {"name": "rect", "x": 10, "y": 20, "width": 5, "height": 10},
-            ...                 "region_attributes": {"label": "cat"},
+            ...                 "region_attributes": {"label": lb},
             ...         }]
-            ...     }, "image_2.png": {
-            ...         "filename": "image_2.png",
-            ...         "regions": [{
-            ...                 "shape_attributes": {"name": "rect", "x": 20, "y": 30, "width": 10, "height": 10},
-            ...                 "region_attributes": {"label": "dog"},
-            ...         }]
-            ...     }, "image_3.png": {
-            ...         "filename": "image_3.png",
-            ...         "regions": [{
-            ...                 "shape_attributes": {"name": "rect", "x": 10, "y": 20, "width": 5, "height": 25},
-            ...                 "region_attributes": {"label": "cat"},
-            ...         }]
-            ...     }
+            ...     } for i, lb in enumerate(["cat", "dog", "cat"])
             ... }
             >>> with open("train_annotations.json", "w") as annotation_file:
             ...     json.dump(annotations, annotation_file)
