1bcb86bb199fda6d94bbb8fdcfd9ecb5e281f730
Ethan Harris <ethanwharris@gmail.com>
2022-02-23 10:03:14 +0000

    Fix

--- flash_examples/video_classification.py
+++ flash_examples/video_classification.py
@@ -34,7 +34,7 @@ datamodule = VideoClassificationData.from_folders(
 model = VideoClassifier(backbone="x3d_xs", labels=datamodule.labels, pretrained=False)
 
 # 3. Create the trainer and finetune the model
-trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count(), fast_dev_run=True)
+trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count())
 trainer.finetune(model, datamodule=datamodule, strategy="freeze")
 
 # 4. Make a prediction
