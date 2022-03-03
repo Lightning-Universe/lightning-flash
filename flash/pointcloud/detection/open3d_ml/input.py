07342d3e423b829ebdc548f2f69a3a9176b7ca28
Jirka Borovec <Borda@users.noreply.github.com>
2022-03-03 14:05:51 +0100

    add class typing (#1211)
    Co-authored-by: Ethan Harris <ethanwharris@gmail.com>

--- flash/pointcloud/detection/open3d_ml/input.py
+++ flash/pointcloud/detection/open3d_ml/input.py
@@ -49,6 +49,8 @@ class BasePointCloudObjectDetectorLoader:
 
 
 class KITTIPointCloudObjectDetectorLoader(BasePointCloudObjectDetectorLoader):
+    meta: dict
+
     def __init__(
         self,
         image_size: tuple = (375, 1242),
