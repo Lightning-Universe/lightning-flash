da4ecbae6ed792f5df999cf49b1cc880d9834d17
Ethan Harris <ethanwharris@gmail.com>
2022-03-29 18:29:01 +0100

    Clean

--- tests/helpers/decorators.py
+++ tests/helpers/decorators.py
@@ -11,12 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import functools
-import multiprocessing as mp
 import os
 
 import pytest
-from dill import dumps, loads
 
 
 def forked(callable):
@@ -25,34 +22,3 @@ def forked(callable):
         return callable
     os.environ["MKL_SERVICE_FORCE_INTEL"] = "1"
     return pytest.mark.forked(callable)
-
-
-class pickleable_target:
-    def __init__(self, target):
-        self.target = target
-
-    def __call__(self, *args, **kwargs):
-        return self.target(*args, **kwargs)
-
-    def __getstate__(self):
-        self.target = dumps(self.target)
-        return self.__dict__
-
-    def __setstate__(self, d):
-        self.__dict__ = d
-        self.target = loads(self.target)
-
-
-class spawned:
-    def __init__(self, target):
-        self.target = target
-        functools.update_wrapper(self, target)
-
-    def __call__(self, *args, **kwargs):
-        context = mp.get_context("spawn")
-        target = pickleable_target(self.target)
-
-        p = context.Process(target=target, args=args, kwargs=kwargs)
-        p.start()
-        p.join()
-        assert not p.exitcode
