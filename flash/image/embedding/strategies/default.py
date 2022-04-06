04b0235079cf2e53fe0bbd49a86ef9212c2d3b6d
Masahiro Wada <argon.argon.argon@gmail.com>
2022-04-06 14:58:02 +0000

    Remove unused head argument

--- flash/image/embedding/strategies/default.py
+++ flash/image/embedding/strategies/default.py
@@ -28,11 +28,10 @@ class DefaultAdapter(Adapter):
 
     required_extras: str = "image"
 
-    def __init__(self, backbone: torch.nn.Module, head: torch.nn.Module):
+    def __init__(self, backbone: torch.nn.Module):
         super().__init__()
 
         self.backbone = backbone
-        self.head = head
 
     @classmethod
     @catch_url_error
@@ -41,10 +40,9 @@ class DefaultAdapter(Adapter):
         *args,
         task: AdapterTask,
         backbone: torch.nn.Module,
-        head: torch.nn.Module,
         **kwargs,
     ) -> Adapter:
-        adapter = cls(backbone, head)
+        adapter = cls(backbone)
         adapter.__dict__["_task"] = task
         return adapter
 
