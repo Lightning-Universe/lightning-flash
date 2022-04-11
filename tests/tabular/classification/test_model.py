46f49f8c9428835c4d0586f16b98f573d828a270
Ethan Harris <ethanwharris@gmail.com>
2022-04-11 13:57:15 +0100

    Add tabular test harnesses (#1285)

--- tests/tabular/classification/test_model.py
+++ tests/tabular/classification/test_model.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import re
+from typing import Any
 from unittest import mock
 
 import pandas as pd
@@ -19,11 +19,11 @@ import pytest
 import torch
 from pytorch_lightning import Trainer
 
-from flash.__main__ import main
 from flash.core.data.io.input import DataKeys
 from flash.core.utilities.imports import _SERVE_TESTING, _TABULAR_AVAILABLE, _TABULAR_TESTING
 from flash.tabular.classification.data import TabularClassificationData
 from flash.tabular.classification.model import TabularClassifier
+from tests.helpers.task_tester import TaskTester
 
 # ======== Mock functions ========
 
@@ -45,6 +45,37 @@ class DummyDataset(torch.utils.data.Dataset):
 
 
 # ==============================
+
+
+class TestTabularClassifier(TaskTester):
+
+    task = TabularClassifier
+    task_kwargs = {
+        "parameters": {"categorical_fields": list(range(4))},
+        "embedding_sizes": [(10, 32) for _ in range(4)],
+        "cat_dims": [10 for _ in range(4)],
+        "num_features": 8,
+        "num_classes": 10,
+        "backbone": "tabnet",
+    }
+    cli_command = "tabular_classification"
+    is_testing = _TABULAR_TESTING
+    is_available = _TABULAR_AVAILABLE
+
+    # TODO: Resolve JIT issues
+    scriptable = False
+    traceable = False
+
+    @property
+    def example_forward_input(self):
+        return {
+            "continuous": torch.rand(1, 4),
+            "categorical": torch.randint(0, 10, size=(1, 4)),
+        }
+
+    def check_forward_output(self, output: Any):
+        assert isinstance(output, torch.Tensor)
+        assert output.shape == torch.Size([1, 10])
 
 
 @pytest.mark.skipif(not _TABULAR_TESTING, reason="tabular libraries aren't installed.")
@@ -105,53 +136,6 @@ def test_init_train_no_cat(backbone, tmpdir):
     trainer.fit(model, train_dl)
 
 
-@pytest.mark.skipif(_TABULAR_AVAILABLE, reason="tabular libraries are installed.")
-def test_module_import_error(tmpdir):
-    data_properties = {
-        "parameters": {"categorical_fields": list(range(16))},
-        "embedding_sizes": [(10, 32) for _ in range(16)],
-        "cat_dims": [10 for _ in range(16)],
-        "num_features": 32,
-        "num_classes": 10,
-        "backbone": "tabnet",
-    }
-    with pytest.raises(ModuleNotFoundError, match="[tabular]"):
-        TabularClassifier(**data_properties)
-
-
-@pytest.mark.skipif(not _TABULAR_TESTING, reason="tabular libraries aren't installed.")
-@pytest.mark.parametrize(
-    "backbone", ["tabnet", "tabtransformer", "fttransformer", "autoint", "node", "category_embedding"]
-)
-def test_jit(backbone, tmpdir):
-    data_properties = {
-        "parameters": {"categorical_fields": list(range(4))},
-        "embedding_sizes": [(10, 32) for _ in range(4)],
-        "cat_dims": [10 for _ in range(4)],
-        "num_features": 8,
-        "num_classes": 10,
-        "backbone": backbone,
-    }
-    model = TabularClassifier(**data_properties)
-    model.eval()
-
-    # torch.jit.script doesn't work with tabnet
-    batch = {
-        "continuous": torch.rand(1, 4),
-        "categorical": torch.randint(0, 10, size=(1, 4)),
-    }
-    model = torch.jit.trace(model, batch, check_trace=False)
-
-    # TODO: torch.jit.save doesn't work with tabnet
-    # path = os.path.join(tmpdir, "test.pt")
-    # torch.jit.save(model, path)
-    # model = torch.jit.load(path)
-
-    out = model(batch)
-    assert isinstance(out, torch.Tensor)
-    assert out.shape == torch.Size([1, 10])
-
-
 @pytest.mark.skipif(not _SERVE_TESTING, reason="serve libraries aren't installed.")
 @pytest.mark.parametrize(
     "backbone", ["tabnet", "tabtransformer", "fttransformer", "autoint", "node", "category_embedding"]
@@ -169,19 +153,3 @@ def test_serve(backbone):
     model = TabularClassifier.from_data(datamodule=datamodule, backbone=backbone)
     model.eval()
     model.serve(parameters=datamodule.parameters)
-
-
-@pytest.mark.skipif(_TABULAR_AVAILABLE, reason="tabular libraries are installed.")
-def test_load_from_checkpoint_dependency_error():
-    with pytest.raises(ModuleNotFoundError, match=re.escape("'lightning-flash[tabular]'")):
-        TabularClassifier.load_from_checkpoint("not_a_real_checkpoint.pt")
-
-
-@pytest.mark.skipif(not _TABULAR_TESTING, reason="tabular libraries aren't installed.")
-def test_cli():
-    cli_args = ["flash", "tabular_classification", "--trainer.fast_dev_run", "True"]
-    with mock.patch("sys.argv", cli_args):
-        try:
-            main()
-        except SystemExit:
-            pass
