bbebd11a8614b05a289a28a9f57169014177447f
Ethan Harris <ethanwharris@gmail.com>
2022-04-19 20:03:18 +0100

    Add test harness fit test to graph tasks

--- flash/graph/classification/input_transform.py
+++ flash/graph/classification/input_transform.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, List
-
-from torch.utils.data.dataloader import default_collate
+from typing import Any, Callable, Dict
 
 from flash.core.data.io.input import DataKeys
 from flash.core.data.io.input_transform import InputTransform
@@ -22,7 +20,7 @@ from flash.core.data.utilities.samples import to_sample
 from flash.core.utilities.imports import _GRAPH_AVAILABLE
 
 if _GRAPH_AVAILABLE:
-    from torch_geometric.data import Batch, Data
+    from torch_geometric.data import Data
     from torch_geometric.transforms import NormalizeFeatures
 else:
     Data = object
@@ -46,16 +44,5 @@ class PyGTransformAdapter:
 
 
 class GraphClassificationInputTransform(InputTransform):
-    @staticmethod
-    def _pyg_collate(samples: List[Dict[str, Any]]) -> Dict[str, Any]:
-        inputs = Batch.from_data_list([sample[DataKeys.INPUT] for sample in samples])
-        if DataKeys.TARGET in samples[0]:
-            targets = default_collate([sample[DataKeys.TARGET] for sample in samples])
-            return {DataKeys.INPUT: inputs, DataKeys.TARGET: targets}
-        return {DataKeys.INPUT: inputs}
-
-    def collate(self) -> Callable:
-        return self._pyg_collate
-
     def per_sample_transform(self) -> Callable:
         return PyGTransformAdapter(NormalizeFeatures())
