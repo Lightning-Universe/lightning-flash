a8d6be4adb6bd51fe8ff6642944f8d66b8b6d72a
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2022-04-28 21:24:31 +0530

    Update conf.py with lightning extension (#1317)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,7 +121,7 @@ extensions = [
     "sphinx_copybutton",
     "sphinx_paramlinks",
     "sphinx_togglebutton",
-    "pt_lightning_sphinx_theme.extensions.lightning_tutorials",
+    "pt_lightning_sphinx_theme.extensions.lightning",
 ]
 
 # autodoc: Default to members and undoc-members
