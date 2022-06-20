f6b17c3dbf7439cb34710c06629b565faeba036a
Jirka <jirka.borovec@seznam.cz>
2022-06-21 01:50:31 +0200

    just floats

--- flash/tabular/classification/utils.py
+++ flash/tabular/classification/utils.py
@@ -33,8 +33,8 @@ def _impute(df: DataFrame, num_cols: List) -> DataFrame:
 
 
 def _compute_normalization(df: DataFrame, num_cols: List) -> Tuple:
-    df_mean = {c: np.nanmean(df[c], dtype=float).astype(df[c].dtype) for c in num_cols}
-    df_std = {c: np.nanstd(df[c], dtype=float).astype(df[c].dtype) for c in num_cols}
+    df_mean = {c: np.nanmean(df[c], dtype=float) for c in num_cols}
+    df_std = {c: np.nanstd(df[c], dtype=float) for c in num_cols}
     zero_std = [c for c in num_cols if df_std[c] == 0]
     if zero_std:
         logging.warning(
