da472bf2488b543c16e31c16ee575918e6ec6b85
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-07-13 11:00:07 -0400

    update test

--- tests/core/data/utilities/test_paths.py
+++ tests/core/data/utilities/test_paths.py
@@ -1,5 +1,3 @@
-# TODO add tests for filter_valid_files
-
 import os
 from pathlib import Path
 from string import ascii_lowercase
@@ -18,20 +16,20 @@ PATH_TYPE = Union[str, bytes, os.PathLike]
 
 
 def _make_mock_dir(mock_files: List) -> List[PATH_TYPE]:
-    temp_path = os.path.join(PARENTDIR, "tmp")
-    mock_dir = []
+    tmpdir = os.path.join(PARENTDIR, "tmp")
+    mockdir = []
     for idx, f_ext in enumerate(mock_files):
-        mock_dir.append(os.path.join(temp_path, mock_files[idx]))
-    return mock_dir
+        mockdir.append(os.path.join(tmpdir, mock_files[idx]))
+    return mockdir
 
 
-def _make_mock_names(mock_extensions) -> List[str]:
-    mock_files = mock_extensions[:]
-    for idx, f_ext in enumerate(mock_files):
+def _make_fake_files(mock_extensions) -> List[str]:
+    fake_files = mock_extensions[:]
+    for idx, f_ext in enumerate(fake_files):
         idxs = random.randint(0, len(ascii_lowercase), size=3)
         fake_name = "".join([ascii_lowercase[ix] for ix in idxs])
-        mock_files[idx] = "".join([fake_name, f_ext])
-    return mock_files
+        fake_files[idx] = "".join([fake_name, f_ext])
+    return fake_files
 
 
 def _make_fake_extensions() -> List[str]:
@@ -52,9 +50,7 @@ def test_filter_valid_files() -> None:
     valid_extensions = list(valid_extensions)
     fake_extensions = _make_fake_extensions()
     mock_files = valid_extensions + fake_extensions
-    mock_files = _make_mock_names(mock_files)
+    mock_files = _make_fake_files(mock_files)
     mockdir = _make_mock_dir(mock_files)
     filtered = filter_valid_files(files=mockdir, valid_extensions=valid_extensions)
     assert all(i not in fake_extensions for i in filtered)
-
-
