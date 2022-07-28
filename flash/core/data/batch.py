504b2ef8b17cc8f5bdcb6f7a62e320abcec89ad0
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-07-28 12:09:55 +0530

    pep8 fix

--- flash/core/data/batch.py
+++ flash/core/data/batch.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 from typing import Any, Callable, List, TYPE_CHECKING
 
-import torch.nn
+from torch import nn
 
 from flash.core.data.utilities.classification import _is_list_like
 
