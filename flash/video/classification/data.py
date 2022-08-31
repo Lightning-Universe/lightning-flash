da2fe56dbf385def214e0c82f5bb660a9fbcf33c
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-08-31 12:48:45 +0530

    Fix doctest

--- flash/video/classification/data.py
+++ flash/video/classification/data.py
@@ -641,7 +641,7 @@ class VideoClassificationData(DataModule):
             >>> datamodule.num_classes
             2
             >>> datamodule.labels
-            ['fruit', 'vegetable', "fruit"]
+            ['fruit', 'vegetable']
             >>> model = VideoClassifier(backbone="x3d_xs", num_classes=datamodule.num_classes)
             >>> trainer = Trainer(fast_dev_run=True)
             >>> trainer.fit(model, datamodule=datamodule)  # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
