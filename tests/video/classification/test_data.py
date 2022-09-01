62e67f5f5e0e09c1fc47b291d106aa087f5fb0b2
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-09-01 11:43:53 +0530

    Add check for video available or not

--- tests/video/classification/test_data.py
+++ tests/video/classification/test_data.py
@@ -34,6 +34,7 @@ def create_dummy_video_frames(num_frames: int, height: int, width: int):
     return torch.stack(data, 0)
 
 
+@pytest.mark.skipif(not _VIDEO_AVAILABLE, reason="PyTorchVideo isn't installed.")
 def temp_encoded_tensors(num_frames: int, height=10, width=10):
     data = create_dummy_video_frames(num_frames, height, width)
     return thwc_to_cthw(data).to(torch.float32)
