8853ae940eefda86c779b4ea39ad5e70ac93c54a
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-09-02 13:34:34 +0530

    Fix

--- tests/tpu/test_tpu_multi_core.py
+++ tests/tpu/test_tpu_multi_core.py
@@ -43,19 +43,15 @@ def test_tpu_finetuning():
 
 @pytest.mark.skipif(not os.getenv("FLASH_RUN_TPU_TESTS", "0") == "1", reason="Should run with TPU test")
 def test_tpu_prediction():
-    boring_model = BoringModel()
-    boring_dm = BoringDataModule()
+    task = TestTaskWithFinetuning(loss_fn=F.nll_loss)
+    dataloader = DataLoader(DummyDataset())
 
     trainer = flash.Trainer(fast_dev_run=True, devices=8, accelerator="tpu")
     assert isinstance(trainer.accelerator, TPUAccelerator)
 
-    trainer.fit(model=boring_model, datamodule=boring_dm)
+    trainer.fit(model=task, train_dataloader=dataloader)
     _assert_state_finished(trainer, "fit")
-    trainer.validate(model=boring_model, datamodule=boring_dm)
-    _assert_state_finished(trainer, "validate")
-    trainer.test(model=boring_model, datamodule=boring_dm)
-    _assert_state_finished(trainer, "test")
 
     with pytest.raises(NotImplementedError, match="not supported"):
-        trainer.predict(model=boring_model, datamodule=boring_dm)
+        trainer.predict(model=task, dataloaders=dataloader)
     return
