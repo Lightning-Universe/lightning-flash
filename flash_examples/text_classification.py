21d95f90cb281739ffc64a51032688d6fe8f00e9
Ethan Harris <ethanwharris@gmail.com>
2022-09-30 21:10:50 +0100

    Fixes

--- flash_examples/text_classification.py
+++ flash_examples/text_classification.py
@@ -20,20 +20,20 @@ from flash.text import TextClassificationData, TextClassifier
 # 1. Create the DataModule
 download_data("https://pl-flash-data.s3.amazonaws.com/imdb.zip", "./data/")
 
-# datamodule = TextClassificationData.from_csv(
-#     "review",
-#     "sentiment",
-#     train_file="data/imdb/train.csv",
-#     val_file="data/imdb/valid.csv",
-#     batch_size=4,
-# )
+datamodule = TextClassificationData.from_csv(
+    "review",
+    "sentiment",
+    train_file="data/imdb/train.csv",
+    val_file="data/imdb/valid.csv",
+    batch_size=4,
+)
 
 # 2. Build the task
-model = TextClassifier(backbone="clip_resnet50", num_classes=2)
+model = TextClassifier(backbone="prajjwal1/bert-medium", labels=datamodule.labels)
 
 # 3. Create the trainer and finetune the model
-trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count(), limit_train_batches=10, limit_val_batches=1)
-# trainer.finetune(model, datamodule=datamodule, strategy="freeze")
+trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count())
+trainer.finetune(model, datamodule=datamodule, strategy="freeze")
 
 # 4. Classify a few sentences! How was the movie?
 datamodule = TextClassificationData.from_lists(
@@ -44,11 +44,8 @@ datamodule = TextClassificationData.from_lists(
     ],
     batch_size=4,
 )
-
-embedder = model.as_embedder("adapter.backbone")
-
-predictions = trainer.predict(embedder, datamodule=datamodule)
+predictions = trainer.predict(model, datamodule=datamodule, output="labels")
 print(predictions)
 
 # 5. Save the model!
-# trainer.save_checkpoint("text_classification_model.pt")
+trainer.save_checkpoint("text_classification_model.pt")
